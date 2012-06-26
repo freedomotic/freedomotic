8aaba11335555a670ddc3aca85f5ecf32c1227d6
gpt <gabriel.pulidodetorres@gmail.com>
2012-06-26 18:56:24 +0200

    webserver plugin: first commit for a plugin to serve the gwt webapp server side. As a first implementation the plugin serves the /webapps/gwt_client folder (relative path to the plugin folder)

---
+++ plugins/devices/webserver/src/it/freedomotic/webserver/ApplicationServer.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.freedomotic.webserver;
+
+import it.freedomotic.api.EventTemplate;
+import it.freedomotic.api.Protocol;
+import it.freedomotic.app.Freedomotic;
+import it.freedomotic.exceptions.UnableToExecuteException;
+import it.freedomotic.reactions.Command;
+import it.freedomotic.util.Info;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ *
+ * @author gpt
+ */
+public class ApplicationServer extends Protocol{
+    //TODO: read from config file
+    public static final int PORT = 8080;
+    public static final String WEBAPP_DIR = "/webapps/gwt_client";
+    public static final String WEBAPP_CTX = "/";
+
+    public static Server server;
+    
+    public ApplicationServer() {
+        super("ApplicationServer", "/es.gpulido.webserver/applicationserver-manifest.xml");
+    }
+    
+    @Override
+    public void onStart(){
+         try {
+        String dir = Info.getApplicationPath() + "/plugins/devices/es.gpulido.webserver/data"+WEBAPP_DIR;              
+        server = new Server(PORT);               
+        WebAppContext context = new WebAppContext();        
+        context.setDescriptor(dir+"/WEB-INF/web.xml");
+        context.setResourceBase(dir);
+        context.setContextPath(WEBAPP_CTX);
+        context.setParentLoaderPriority(true);  
+        server.setHandler(context);                 
+       
+            server.start();            
+        } catch (Exception ex) {
+            Logger.getLogger(ApplicationServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    @Override
+    public void onStop(){
+        try {
+            server.stop();
+        } catch (Exception ex) {
+            Logger.getLogger(ApplicationServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    
+    @Override
+    protected void onRun() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void onCommand(Command c) throws IOException, UnableToExecuteException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected boolean canExecute(Command c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void onEvent(EventTemplate event) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ plugins/devices/webserver/src/it/freedomotic/webserver/ApplicationServerMainTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.freedomotic.webserver;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ *
+ * @author gpt
+ */
+public class ApplicationServerMainTest {
+        public static void main(String[] args) throws Exception {
+       
+            Server server = new Server(8080);
+            String dir= "/home/gpt/Desarrollo/freedomotic/framework/freedomotic/plugins/devices/es.gpulido.webserver/data/webapps/gwt_client";
+        WebAppContext context = new WebAppContext();
+        
+        context.setDescriptor(dir+"/WEB-INF/web.xml");
+        context.setResourceBase("/home/gpt/Desarrollo/freedomotic/framework/freedomotic/plugins/devices/es.gpulido.webserver/data/webapps/gwt_client");
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
+ 
+        server.setHandler(context);
+        server.start();
+            
+            
+        }
+}
