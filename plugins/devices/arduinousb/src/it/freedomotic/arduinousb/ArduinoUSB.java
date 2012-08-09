7ea75772276e874f840880fdf8101c3eb0afa31f
Mauro Cicolella <mcicolella@libero.it>
2012-08-09 22:48:42 +0200

    arduinousb - added a new skeleton plugin for arduino serial protocol integration

---
+++ plugins/devices/arduinousb/src/it/freedomotic/arduinousb/ArduinoUSB.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.freedomotic.arduinousb;
+
+import it.freedomotic.api.EventTemplate;
+import it.freedomotic.api.Protocol;
+import it.freedomotic.app.Freedomotic;
+import it.freedomotic.exceptions.UnableToExecuteException;
+import it.freedomotic.reactions.Command;
+import it.freedomotic.serial.SerialConnectionProvider;
+import it.freedomotic.serial.SerialDataConsumer;
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Mauro Cicolella
+ */
+public class ArduinoUSB extends Protocol implements SerialDataConsumer {
+
+    SerialConnectionProvider serial;
+
+    public ArduinoUSB() {
+        super("Arduino USB", "/it.freedomotic.arduinousb/arduinousb-manifest.xml");
+        setPollingWait(2000); //waits 2000ms in onRun method before call onRun() again
+    }
+
+    @Override
+    public void onStart() {
+        //called when the user starts the plugin from UI
+        if (serial == null) {
+            serial = new SerialConnectionProvider();
+            //UNCOMMENT IF NEEDED:
+            //instead of specify a port name is also possible to search for 
+            //the right port using an hello message and an expected reply
+            //the hello message will be broadcasted to all usb connected devices
+            //serial.setAutodiscover(
+              //      configuration.getStringProperty("serial.hello", "hello"), 
+                //    configuration.getStringProperty("serial.hello-reply", "hello-reply"));
+            //connection parameters
+            serial.setPortName(configuration.getStringProperty("serial.port", "/dev/usb0"));
+            serial.setPortBaudrate(configuration.getIntProperty("serial.baudrate", 9600));
+            serial.setPortDatabits(configuration.getIntProperty("serial.databits", 8));
+            serial.setPortParity(configuration.getIntProperty("serial.parity", 0));
+            serial.setPortStopbits(configuration.getIntProperty("serial.stopbits", 1));
+
+        }
+    }
+
+    @Override
+    public void onStop() {
+        //called when the user stops the plugin from UI
+        if (serial != null) {
+            serial.disconnect();
+        }
+    }
+
+    @Override
+    protected void onRun() {
+        //called in a loop while this plugin is running
+        //loops waittime is specified using setPollingWait()
+        try {
+            //sends the string to serialport and waits the amount of time
+            //written in setPollingWait() method [you can found it in Massabus constructor]
+            String message = "A_STRING_MESSAGE";
+            String reply = serial.send(message);
+            Freedomotic.logger.config("Arduino USB replies " + reply + " to message " + message);
+        } catch (IOException ex) {
+            setDescription("Stopped for IOException in onRun"); //write here a better error message for the user
+            stop();
+
+        }
+    }
+
+    @Override
+    protected void onCommand(Command c) throws IOException, UnableToExecuteException {
+        //this method receives freedomotic commands send on channel app.actuators.protocol.arduinousb.in
+        String message = c.getProperty("arduinousb.message");
+        String reply = null;
+        try {
+            reply = serial.send(message);
+        } catch (IOException iOException) {
+            setDescription("Stopped for IOException in onCommand"); //write here a better error message for the user
+            stop();
+        }
+        Freedomotic.logger.config("Arduino USB replies " + reply + " after executing command " + c.getName());
+    }
+
+    @Override
+    protected boolean canExecute(Command c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void onEvent(EventTemplate event) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onDataAvailable(String data) {
+        //called when something is readed from the serial port
+        Freedomotic.logger.config("Arduino USB reads '" + data + "'");
+    }
+}
