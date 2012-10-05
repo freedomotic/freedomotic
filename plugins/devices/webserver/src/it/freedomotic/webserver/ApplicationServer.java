b78a6cec3b89ca594cfa886bbc5b9dfb960b851b
Enrico Nicoletti <enrico.nicoletti84@gmail.com>
2012-10-05 12:53:10 +0200

    webserver - added plugin description "Visit URL_OF_SERVED_WAR"

--- plugins/devices/webserver/src/it/freedomotic/webserver/ApplicationServer.java
+++ plugins/devices/webserver/src/it/freedomotic/webserver/ApplicationServer.java
@@ -6,78 +6,83 @@ package it.freedomotic.webserver;
 
 import it.freedomotic.api.EventTemplate;
 import it.freedomotic.api.Protocol;
-import it.freedomotic.app.Freedomotic;
 import it.freedomotic.exceptions.UnableToExecuteException;
 import it.freedomotic.reactions.Command;
 import it.freedomotic.util.Info;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  *
  * @author gpt
  */
-public class ApplicationServer extends Protocol{
+public class ApplicationServer extends Protocol {
     //TODO: read from config file
+
     int port;// = 8080;
     String webapp_dir;
     String war_file;
-    public static final String WEBAPP_CTX = "/";    
-
+    public static final String WEBAPP_CTX = "/";
     public static Server server;
-    
+
     public ApplicationServer() {
         super("ApplicationServer", "/es.gpulido.webserver/applicationserver-manifest.xml");
-        
+
         port = configuration.getIntProperty("PORT", 8080);
-        webapp_dir = configuration.getStringProperty("WEBAPP_DIR","/webapps/gwt_client");
-        war_file=configuration.getStringProperty("WAR_FILE","Freedomotic.war");
+        webapp_dir = configuration.getStringProperty("WEBAPP_DIR", "/webapps/gwt_client");
+        war_file = configuration.getStringProperty("WAR_FILE", "Freedomotic.war");
         //TODO: check that the war file is correct.
     }
-    
+
     @Override
-    public void onStart(){
-         try {
-        String dir = Info.getApplicationPath() + "/plugins/devices/es.gpulido.webserver/data"+webapp_dir;              
-        server = new Server(port);               
+    public void onStart() {
+        try {
+            String dir = Info.getApplicationPath() + "/plugins/devices/es.gpulido.webserver/data" + webapp_dir;
+            server = new Server(port);
 //        WebAppContext context = new WebAppContext();        
 //        context.setDescriptor(dir+"/WEB-INF/web.xml");
 //        context.setResourceBase(dir);
 //        context.setContextPath(WEBAPP_CTX);
 //        context.setParentLoaderPriority(true);  
 //        server.setHandler(context);
-        
-        
-        WebAppContext webapp = new WebAppContext();
-        webapp.setContextPath(WEBAPP_CTX);
-        webapp.setWar(dir+"/"+war_file);
-        server.setHandler(webapp);
-        
-        
-       
-            server.start();            
+
+
+            WebAppContext webapp = new WebAppContext();
+            webapp.setContextPath(WEBAPP_CTX);
+            webapp.setWar(dir + "/" + war_file);
+            server.setHandler(webapp);
+            
+            //print the URL to visit as plugin description
+            InetAddress addr = InetAddress.getLocalHost();
+            String hostname = addr.getHostName();
+            //strip away the '.war' extension and put all togheter
+            URL url = new URL("http://"+hostname+":"+port+"/"+war_file.substring(0, war_file.lastIndexOf(".")));
+            setDescription("Visit " + url.toString());
+
+
+            server.start();
         } catch (Exception ex) {
             Logger.getLogger(ApplicationServer.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
+
     }
-    
+
     @Override
-    public void onStop(){
+    public void onStop() {
         try {
             server.stop();
+            setDescription(configuration.getStringProperty("description", this.getName()));
         } catch (Exception ex) {
             Logger.getLogger(ApplicationServer.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
+
     }
-    
-    
+
     @Override
     protected void onRun() {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -97,5 +102,4 @@ public class ApplicationServer extends Protocol{
     protected void onEvent(EventTemplate event) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
 }
