bf146d94f4737bea515e86db411a7ba0fb00992f
Enrico Nicoletti <enrico.nicoletti84@gmail.com>
2012-12-01 10:55:19 +0100

    p2p - changed broker default configuration to peer to peer messaging (can run more instances at the same time and exchange messages, but no data)

--- framework/freedomotic/src/it/freedomotic/util/Info.java
+++ framework/freedomotic/src/it/freedomotic/util/Info.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.StringTokenizer;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,7 +29,11 @@ public class Info {
     public static final File PATH_PROVIDERS_FOLDER = new File(PATH_WORKDIR + "/plugins/providers/");
     //framework API and messaging
     public static final String BROKER_IP = getLocalHost();
-    public static final String BROKER_DEFAULT = "vm://freedomotic";
+    //http://activemq.apache.org/activemq-3-transport-configurations.html
+    //http://marcelojabali.blogspot.it/2011/10/apache-activemq-enhancements-to-jms.html
+    //public static final String BROKER_DEFAULT = "vm://freedomotic";
+    //info about peer brokers http://fusesource.com/docs/broker/5.2/connectivity_guide/N04F73598.04EE2290.html
+    public static final String BROKER_DEFAULT = "peer://freedomotic/broker"+UUID.randomUUID();
     public static final int    BROKER_PORT = 61616;
     public static final String BROKER_STOMP = "stomp://0.0.0.0:61666";
     public static final String BROKER_WEBSOCKET = "ws://0.0.0.0:61614";
