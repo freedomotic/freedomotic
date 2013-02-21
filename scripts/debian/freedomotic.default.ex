ea71ecd8b9796a848cecf1b1ae702ae9a83fbb5a
Gabriel Pulido <gabriel.pulidodetorres@gmail.com>
2013-02-21 21:06:05 +0100

    Freedomotic-core: Included a debian package generation scripts and files.
    Call the target "create-release" passing the parameter "generate_deb=tru" to the ant process to genererate debian packages.

---
+++ scripts/debian/freedomotic.default.ex
@@ -0,0 +1,10 @@
+# Defaults for freedomotic initscript
+# sourced by /etc/init.d/freedomotic
+# installed at /etc/default/freedomotic by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ scripts/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(freedomotic):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="freedomotic" command="/usr/bin/freedomotic"
