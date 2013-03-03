3e243786828ebe1d792035e76aff9e8fd7b3f5af
Gabriel Pulido <gabriel.pulidodetorres@gmail.com>
2013-03-03 18:57:29 +0100

    Core: Update versions to the new dev cycle Bender

--- framework/freedomotic/src/it/freedomotic/util/Info.java
+++ framework/freedomotic/src/it/freedomotic/util/Info.java
@@ -11,9 +11,9 @@ import java.util.logging.Logger;
 public class Info {
     //framework versioning
     public static final Integer FRAMEWORK_MAJOR = 5;
-    public static final Integer FRAMEWORK_MINOR = 4;
+    public static final Integer FRAMEWORK_MINOR = 5;
     public static final Integer FRAMEWORK_BUILD = 0;
-    public static final String  FRAMEWORK_VERSION_CODENAME = "Andrew";
+    public static final String  FRAMEWORK_VERSION_CODENAME = "Bender";
     public static final String  FRAMEWORK_RELEASE_DATE = "March 2013";
     public static final String  FRAMEWORK_LICENSE = "GNU Generic Public License v2";
     public static final String  FRAMEWORK_RELEASE_TYPE = "beta";
