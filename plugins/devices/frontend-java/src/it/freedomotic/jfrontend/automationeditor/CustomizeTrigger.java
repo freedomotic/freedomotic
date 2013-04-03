af40b3929838472bae33f938f4a84fed31c6868d
Matteo Mazzoni <matteo@bestmazzo.it>
2013-04-03 17:43:43 +0200

    Enhancements on JFrontend:
    SOLVED Core-253: a confirm is required before deleting an object
    
    SOLVED Core-256
     - added CHANNEL, SUSPENSION TIME, DELAY, MAX EXECUTIONS fields to CustomizeTrigger.
     - added support for managing empty payload lines: if a cell is empty, related line is not added
    
    Localization: localized more fields

--- plugins/devices/frontend-java/src/it/freedomotic/jfrontend/automationeditor/CustomizeTrigger.java
+++ plugins/devices/frontend-java/src/it/freedomotic/jfrontend/automationeditor/CustomizeTrigger.java
@@ -15,6 +15,7 @@ import it.freedomotic.reactions.Payload;
 import it.freedomotic.reactions.Statement;
 import it.freedomotic.reactions.Trigger;
 import it.freedomotic.reactions.TriggerPersistence;
+import it.freedomotic.util.i18n;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -42,6 +43,10 @@ public class CustomizeTrigger extends javax.swing.JFrame {
         this.setTitle(t.getName() + " Trigger Editor");
         txtName.setText(t.getName());
         txtDescription.setText(t.getDescription());
+        txtChannel.setText(t.getChannel());
+        txtSuspTime.setText(Long.toString(t.getSuspensionTime())) ;
+        txtDelay.setText(Integer.toString(t.getDelay()));
+        txtMaxExTimes.setText(Long.toString(t.getMaxExecutions()));
         lblExplanation.setText("Fire this trigger if parameters are:");
 
         if (t.isHardwareLevel() || !t.isToPersist()) {
@@ -73,10 +78,21 @@ public class CustomizeTrigger extends javax.swing.JFrame {
     }
 
     private void save(Trigger t) {
-        table.getCellEditor().stopCellEditing();
+        if (table.getCellEditor() != null) {
+            table.getCellEditor().stopCellEditing();
+        }
+        
         Payload payload = new Payload();
         for (int r = 0; r < model.getRowCount(); r++) {
-            payload.addStatement(
+            boolean saveCurrent = true;
+            for (int k = 0;k<4;k++){
+                String test = model.getValueAt(r, 0).toString();
+                if (test == null || test.equals("")){
+                    saveCurrent = false;
+                    break;
+                }
+            }  
+            if (saveCurrent) payload.addStatement(
                     model.getValueAt(r, 0).toString(),
                     model.getValueAt(r, 1).toString(),
                     model.getValueAt(r, 2).toString(),
@@ -84,9 +100,11 @@ public class CustomizeTrigger extends javax.swing.JFrame {
 
         }
         t.setName(txtName.getText());
-        t.setDescription(txtDescription.getName());
-        t.setChannel(original.getChannel());
         t.setDescription(txtDescription.getText());
+        t.setChannel(txtChannel.getText());
+        t.setSuspensionTime(Long.parseLong(txtSuspTime.getText()));
+        t.setDelay(Integer.parseInt(txtDelay.getText()));
+        t.setMaxExecutions(Long.parseLong(txtMaxExTimes.getText()));
         t.setPersistence(true);
         t.setPayload(payload);
     }
@@ -112,30 +130,38 @@ public class CustomizeTrigger extends javax.swing.JFrame {
         pnlParam = new javax.swing.JPanel();
         btnAddRow = new javax.swing.JButton();
         lblTemplateWarning = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtChannel = new javax.swing.JTextField();
+        txtSuspTime = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtDelay = new javax.swing.JTextField();
+        txtMaxExTimes = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        btnSave.setText("Save as New Trigger");
+        btnSave.setText(i18n.msg("save_as_new")+i18n.msg("trigger"));
         btnSave.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnSaveActionPerformed(evt);
             }
         });
 
-        jLabel1.setText("Name:");
+        jLabel1.setText(i18n.msg("name")+":");
 
-        jLabel2.setText("Description:");
+        jLabel2.setText(i18n.msg("description")+":");
 
         lblExplanation.setText("Fire the event if this parameters are consistent with the received event:");
 
-        btnEdit.setText("Save Changes");
+        btnEdit.setText(i18n.msg("save"));
         btnEdit.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnEditActionPerformed(evt);
             }
         });
 
-        btnDelete.setText("Delete Trigger");
+        btnDelete.setText(i18n.msg("delete")+i18n.msg("trigger"));
         btnDelete.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnDeleteActionPerformed(evt);
@@ -156,6 +182,14 @@ public class CustomizeTrigger extends javax.swing.JFrame {
 
         lblTemplateWarning.setText("This trigger is a predefined template so you cannot change it. Save it as new trigger.");
 
+        jLabel3.setText(i18n.msg("channel")+":");
+
+        jLabel4.setText(i18n.msg("suspension_time")+":");
+
+        jLabel5.setText(i18n.msg("delay")+":");
+
+        jLabel6.setText(i18n.msg("max_executions")+":");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -164,6 +198,7 @@ public class CustomizeTrigger extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblTemplateWarning, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
@@ -173,25 +208,33 @@ public class CustomizeTrigger extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(btnDelete))
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel2))
-                                .addGap(12, 12, 12)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtDescription)
-                                    .addComponent(txtName, javax.swing.GroupLayout.DEFAULT_SIZE, 323, Short.MAX_VALUE)))
-                            .addGroup(layout.createSequentialGroup()
                                 .addComponent(lblExplanation)
                                 .addGap(18, 18, 18)
-                                .addComponent(btnAddRow)))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(lblTemplateWarning, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addComponent(btnAddRow))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel6))
+                                .addGap(61, 61, 61)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtMaxExTimes, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(txtDescription)
+                                        .addComponent(txtName)
+                                        .addComponent(txtChannel)
+                                        .addComponent(txtSuspTime, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(txtDelay, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addGap(0, 183, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(15, 15, 15)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -199,7 +242,23 @@ public class CustomizeTrigger extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtDescription, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(13, 13, 13)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtChannel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtSuspTime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtDelay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtMaxExTimes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addGap(42, 42, 42)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblExplanation)
                     .addComponent(btnAddRow))
@@ -257,12 +316,20 @@ public class CustomizeTrigger extends javax.swing.JFrame {
     private javax.swing.JButton btnSave;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblExplanation;
     private javax.swing.JLabel lblTemplateWarning;
     private javax.swing.JPanel pnlParam;
+    private javax.swing.JTextField txtChannel;
+    private javax.swing.JTextField txtDelay;
     private javax.swing.JTextField txtDescription;
+    private javax.swing.JTextField txtMaxExTimes;
     private javax.swing.JTextField txtName;
+    private javax.swing.JTextField txtSuspTime;
     // End of variables declaration//GEN-END:variables
 
     private int getOperatorIndex(String operand) {
