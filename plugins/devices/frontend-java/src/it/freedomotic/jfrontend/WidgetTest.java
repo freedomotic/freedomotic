e2bca8cdc71ee6f057531c03c6b78ffc45354661
Matteo Mazzoni <matteo@bestmazzo.it>
2013-04-24 09:25:51 +0200

    i18n: Initial implementation  In order to internationalize a string, you can use the static method i18n.msg()  - i18n.msg(String message) uses core i18n string situated in "freedomotic base path"/i18n/Freedomotic.properties and localized variants  - plugins can contain specific localized strings, to be placed in "plugin dir"/i18n/_last_package_part_.properties and localized variants  - in order to access plugins localized strings the method to use is i18n.msg(Object obj, String message), where obj is a pointer to the local object ("this")

---
+++ plugins/devices/frontend-java/src/it/freedomotic/jfrontend/WidgetTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.freedomotic.jfrontend;
+
+import it.freedomotic.core.ResourcesManager;
+import it.freedomotic.objects.EnvObjectLogic;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ * Only a test to demonstrate how to create custom widgets.
+ * This example draws a red vertical bar at the left side of the object icon.
+ * Refer to ImageDrawer renderObjects method to enable it.
+ * @author enrico
+ */
+public class WidgetTest {
+
+    private final EnvObjectLogic obj;
+
+    public WidgetTest(EnvObjectLogic obj) {
+        this.obj = obj;
+    }
+
+    public synchronized BufferedImage draw() {
+        try {
+            String name = obj.getPojo().getCurrentRepresentation().getIcon();
+            BufferedImage resource = ResourcesManager.getResource(name);
+            Graphics2D canvas = resource.createGraphics();
+            canvas.setColor(Color.red);
+            canvas.fillRect(0, 0,
+                    (int) 10,
+                    (int) resource.getHeight());
+            //ResourcesManager.addResource(name, canvas);
+            return resource;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
