9d91d97935a0d272a13d6d55f6d7ab84ac6923c3
Mauro Cicolella <mcicolella@libero.it>
2013-05-11 14:37:57 +0200

    openpicus grove system plugin ported to 5.5 version - added licence header

--- plugins/devices/openpicus-grove-system/src/it/cicolella/grovesystem/UDPServer.java
+++ plugins/devices/openpicus-grove-system/src/it/cicolella/grovesystem/UDPServer.java
@@ -1,7 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ Copyright FILE Mauro Cicolella, 2012-2013
+
+ This file is part of FREEDOMOTIC.
+
+ FREEDOMOTIC is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ FREEDOMOTIC is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Freedomotic.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package it.cicolella.grovesystem;
 
 import it.freedomotic.app.Freedomotic;
@@ -11,11 +26,6 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-
-/**
- *
- * @author Mauro Cicolella
- */
 
 public class UDPServer extends Thread {
 
