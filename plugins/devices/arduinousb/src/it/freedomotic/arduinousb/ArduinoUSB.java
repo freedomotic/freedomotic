fa8dc838d505dea18328c0958b59ac9e765d8996
Mauro Cicolella <mcicolella@libero.it>
2013-05-11 22:43:21 +0200

    arduinousb plugin ported to 5.5 version - added licence header

--- plugins/devices/arduinousb/src/it/freedomotic/arduinousb/ArduinoUSB.java
+++ plugins/devices/arduinousb/src/it/freedomotic/arduinousb/ArduinoUSB.java
@@ -1,7 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ Copyright FILE Mauro Cicolella, 2012-2013
+
+ This file is part of FREEDOMOTIC.
+
+ FREEDOMOTIC is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ FREEDOMOTIC is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Freedomotic.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package it.freedomotic.arduinousb;
 
 import it.freedomotic.api.EventTemplate;
@@ -13,11 +28,6 @@ import it.freedomotic.serial.SerialConnectionProvider;
 import it.freedomotic.serial.SerialDataConsumer;
 import java.io.IOException;
 
-
-/**
- *
- * @author Mauro Cicolella
- */
 public class ArduinoUSB extends Protocol implements SerialDataConsumer {
 
     SerialConnectionProvider serial;
