37582320ff06741729e6f64b46fcbef330fd994f
Mauro Cicolella <mcicolella@libero.it>
2013-05-11 14:27:08 +0200

    ipx800 plugin ported to 5.5 version - added licence header

--- plugins/devices/ipx800/src/it/cicolella/ipx800/Ipx800.java
+++ plugins/devices/ipx800/src/it/cicolella/ipx800/Ipx800.java
@@ -1,3 +1,22 @@
+/*
+ Copyright FILE Mauro Cicolella, 2012-2013
+
+ This file is part of FREEDOMOTIC.
+
+ FREEDOMOTIC is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ FREEDOMOTIC is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Freedomotic.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.cicolella.ipx800;
 
 import it.freedomotic.api.EventTemplate;
@@ -29,12 +48,6 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-/**
- * A sensor for the board IPX800 developed by www.gce-electronics.com author
- * Mauro Cicolella - www.emmecilab.net For more details please refer to
- * technical forum http://freedomotic.com/forum/6/1031
- *
- */
 public class Ipx800 extends Protocol {
 
     private static ArrayList<Board> boards = null;
