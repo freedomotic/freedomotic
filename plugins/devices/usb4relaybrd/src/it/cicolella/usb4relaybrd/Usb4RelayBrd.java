6e3f627d2bf3b766b513223d0b5f53b97157f480
Mauro Cicolella <mcicolella@libero.it>
2013-05-13 14:43:45 +0200

    usb4relaybrd plugin ported to 5.5 version - added licence header

--- plugins/devices/usb4relaybrd/src/it/cicolella/usb4relaybrd/Usb4RelayBrd.java
+++ plugins/devices/usb4relaybrd/src/it/cicolella/usb4relaybrd/Usb4RelayBrd.java
@@ -1,9 +1,22 @@
-/**
- * A plugin for the Usb4RelayBord by www.denkovi.com 
- * More details at http://www.denkovi.com/product/44/usb-four-4-relay-output-module-board-for-home-automation.html
- * Author Mauro Cicolella
- *
+/*
+ Copyright FILE Mauro Cicolella, 2012-2013
+
+ This file is part of FREEDOMOTIC.
+
+ FREEDOMOTIC is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ FREEDOMOTIC is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Freedomotic.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package it.cicolella.usb4relaybrd;
 
 import it.freedomotic.api.EventTemplate;
