1f7d2d0ae717956f4fc98c78015687ba52fc2e70
Matteo Mazzoni <matteo@bestmazzo.it>
2013-05-15 14:12:32 +0200

    SOLVED: PL-41 - "Webserver plugin: add SSL support"

--- plugins/devices/webserver/src/main/java/it/freedomotic/webserver/ApplicationServer.java
+++ plugins/devices/webserver/src/main/java/it/freedomotic/webserver/ApplicationServer.java
@@ -9,12 +9,15 @@ import it.freedomotic.api.Protocol;
 import it.freedomotic.exceptions.UnableToExecuteException;
 import it.freedomotic.reactions.Command;
 import it.freedomotic.util.Info;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ssl.SslSocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
@@ -29,6 +32,12 @@ public class ApplicationServer extends Protocol {
     String war_file;
     public static final String WEBAPP_CTX = "/";
     public static Server server;
+    private static boolean enableSSL;
+    private static int sslPort;
+    private static String keystorePassword;
+    private static String keystoreFile;
+    private static String plugPath = "/plugins/devices/es.gpulido.webserver/data";
+    private static String keystoreType;
 
     public ApplicationServer() {
         super("ApplicationServer", "/es.gpulido.webserver/applicationserver-manifest.xml");
@@ -36,14 +45,31 @@ public class ApplicationServer extends Protocol {
         port = configuration.getIntProperty("PORT", 8080);
         webapp_dir = configuration.getStringProperty("WEBAPP_DIR", "/webapps/gwt_client");
         war_file = configuration.getStringProperty("WAR_FILE", "Freedomotic.war");
+        enableSSL  =configuration.getBooleanProperty("ENABLE_SSL", true);
+        sslPort= configuration.getIntProperty("SSL_PORT", 8443);
+        keystorePassword = configuration.getStringProperty("KEYSTORE_PASSWORD", "password");
+        keystoreFile= configuration.getStringProperty("KEYSTORE_FILE", "keystore");
+        keystoreType= configuration.getStringProperty("KEYSTORE_TYPE", "JKS");
+        
         //TODO: check that the war file is correct.
     }
 
     @Override
     public void onStart() {
         try {
-            String dir = Info.getApplicationPath() + "/plugins/devices/es.gpulido.webserver/data" + webapp_dir;
+            String dir = Info.getApplicationPath() + plugPath + webapp_dir;
             server = new Server(port);
+            if (enableSSL){
+            SslSocketConnector SSLConnector = new SslSocketConnector();
+            SSLConnector.setPort(sslPort);
+            SSLConnector.setMaxIdleTime(30000);
+            SSLConnector.setKeystore(Info.getApplicationPath() + plugPath + File.separator+ keystoreFile);
+            SSLConnector.setKeyPassword(keystorePassword);
+            SSLConnector.setPassword(keystorePassword);
+            SSLConnector.setKeystoreType(keystoreType);
+            server.addConnector(SSLConnector);
+            
+            }
 //        WebAppContext context = new WebAppContext();        
 //        context.setDescriptor(dir+"/WEB-INF/web.xml");
 //        context.setResourceBase(dir);
@@ -56,12 +82,12 @@ public class ApplicationServer extends Protocol {
             webapp.setContextPath(WEBAPP_CTX);
             webapp.setWar(dir + "/" + war_file);
             server.setHandler(webapp);
-
+            
             //print the URL to visit as plugin description
             InetAddress addr = InetAddress.getLocalHost();
             String hostname = addr.getHostName();
             //strip away the '.war' extension and put all togheter
-            URL url = new URL("http://" + hostname + ":" + port + "/" + war_file.substring(0, war_file.lastIndexOf(".")));
+            URL url = new URL("http://"+hostname+":"+port+"/"+war_file.substring(0, war_file.lastIndexOf(".")));
             setDescription("Visit " + url.toString());
 
 
@@ -85,7 +111,7 @@ public class ApplicationServer extends Protocol {
 
     @Override
     protected void onRun() {
-        // throw new UnsupportedOperationException("Not supported yet.");
+       // throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
