539d939bc5b456fe04c737f3001e6b41902f36b3
Gabriel Pulido <gabriel.pulidodetorres@gmail.com>
2013-07-03 20:35:51 +0200

    Fixing license header for freedomotic-core and freedomotic-model

--- framework/freedomotic-core/src/main/java/it/freedomotic/events/GenericEvent.java
+++ framework/freedomotic-core/src/main/java/it/freedomotic/events/GenericEvent.java
@@ -1,21 +1,23 @@
-/*Copyright 2009 Enrico Nicoletti
-eMail: enrico.nicoletti84@gmail.com
-
-This file is part of Freedomotic.
-
-Freedomotic is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-any later version.
-
-Freedomotic is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with EventEngine; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package it.freedomotic.events;
 
