460310a30fc2eec15f55d5f6a2f337eeb6c35852
Mauro Cicolella <mcicolella@libero.it>
2013-08-26 21:56:05 +0200

    openpicus grove system plugin mavenization. Fixes PL-16

--- plugins/devices/openpicus-grove-system/src/main/java/it/cicolella/grovesystem/GroveSystem.java
+++ plugins/devices/openpicus-grove-system/src/main/java/it/cicolella/grovesystem/GroveSystem.java
@@ -1,21 +1,25 @@
-/*
- Copyright FILE Mauro Cicolella, 2012-2013
-
- This file is part of FREEDOMOTIC.
-
- FREEDOMOTIC is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- FREEDOMOTIC is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Freedomotic.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
+
 package it.cicolella.grovesystem;
 
 import it.freedomotic.api.EventTemplate;
@@ -51,7 +55,7 @@ public class GroveSystem extends Protocol {
      * Initializations
      */
     public GroveSystem() {
-        super("Openpicus Grove System", "/it.cicolella.grovesystem/openpicus-grove-system-manifest.xml");
+        super("Openpicus Grove System", "/grovesystem/openpicus-grove-system-manifest.xml");
         setPollingWait(POLLING_TIME);
     }
 
