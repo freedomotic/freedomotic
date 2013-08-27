1f8b1faf5f68a409b9097e02641ea62a39b88cc6
Mauro Cicolella <mcicolella@libero.it>
2013-08-27 16:06:24 +0200

    mavenization of progettihwsw-eth plugin. Solves PL-23

--- plugins/devices/progettihwsw-eth/src/main/java/it/cicolella/phwsw/ETHProgettiHwSw.java
+++ plugins/devices/progettihwsw-eth/src/main/java/it/cicolella/phwsw/ETHProgettiHwSw.java
@@ -1,22 +1,25 @@
-
-/*
- Copyright FILE Mauro Cicolella, 2012-2013
-
- This file is part of FREEDOMOTIC.
-
- FREEDOMOTIC is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- FREEDOMOTIC is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Freedomotic.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
+
 package it.cicolella.phwsw;
 
 import it.freedomotic.api.EventTemplate;
@@ -60,7 +63,7 @@ public class ETHProgettiHwSw extends Protocol {
      * Initializations
      */
     public ETHProgettiHwSw() {
-        super("ETH Progetti HwSw", "/it.cicolella.phwsw/progettihwsw-manifest.xml");
+        super("ETH Progetti HwSw", "/phwsw/progettihwsw-manifest.xml");
         setPollingWait(POLLING_TIME);
     }
 
