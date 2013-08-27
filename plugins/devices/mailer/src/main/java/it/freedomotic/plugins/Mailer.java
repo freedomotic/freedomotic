74ea0ed793cd60889ea648798c9ca232ecce2f7c
Mauro Cicolella <mcicolella@libero.it>
2013-08-27 18:36:49 +0200

    mavenization of mailer plugin. Fixes PL-33

--- plugins/devices/mailer/src/main/java/it/freedomotic/plugins/Mailer.java
+++ plugins/devices/mailer/src/main/java/it/freedomotic/plugins/Mailer.java
@@ -1,7 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
+
 package it.freedomotic.plugins;
 
 import it.freedomotic.api.EventTemplate;
@@ -28,7 +46,7 @@ public final class Mailer extends Protocol {
     private int sentMails;
 
     public Mailer() {
-        super("Mailer", "/com.freedomotic.mailer/mailer.xml");
+        super("Mailer", "/mailer/mailer.xml");
         addEventListener("app.event.sensor.messages.mail");
     }
 
