2420bf63896f557a15c041d12fdd767702c476ab
Mauro Cicolella <mcicolella@libero.it>
2013-08-28 18:42:23 +0200

    mavenization of x10 plugin. Fixes PL-24

--- plugins/devices/x10/src/main/java/it/freedomotic/plugins/x10/X10.java
+++ plugins/devices/x10/src/main/java/it/freedomotic/plugins/x10/X10.java
@@ -1,3 +1,26 @@
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+
 package it.freedomotic.plugins.x10;
 
 import it.freedomotic.plugins.x10.gateways.PMix35Gateway;
@@ -19,7 +42,7 @@ public class X10 extends Protocol {
     ArrayList<X10AbstractGateway> gateways = new ArrayList<X10AbstractGateway>();
 
     public X10() {
-        super("X10", "/it.nicoletti.x10/x10-manifest.xml");
+        super("X10", "/x10/x10-manifest.xml");
     }
 
     @Override
