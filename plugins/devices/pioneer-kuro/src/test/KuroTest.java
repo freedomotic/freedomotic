4f1aca7a2a5acfd91fa11672848f8486b9d24abb
Mauro Cicolella <mcicolella@libero.it>
2013-09-04 17:12:56 +0200

    mavenization of pioneer-kuro plugin. Fixes PL-27

--- plugins/devices/pioneer-kuro/src/test/KuroTest.java
+++ plugins/devices/pioneer-kuro/src/test/KuroTest.java
@@ -1,7 +1,25 @@
-/*  
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
+
 //import gnu.io.SerialPort;
 //import gnu.io.SerialPort;
 import it.freedomotic.serial.SerialConnectionProvider;
@@ -16,6 +34,7 @@ import org.junit.Test;
  *
  * @author gpt
  */
+
 public class KuroTest {
 
     public KuroTest() {
