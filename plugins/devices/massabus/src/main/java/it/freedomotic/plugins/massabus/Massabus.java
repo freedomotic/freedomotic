49a964efb5f28808c47076c09757aedc0d40081a
Mauro Cicolella <mcicolella@libero.it>
2013-09-09 15:50:12 +0200

    mavenization of massabus plugin. Fixes PL-65

--- plugins/devices/massabus/src/main/java/it/freedomotic/plugins/massabus/Massabus.java
+++ plugins/devices/massabus/src/main/java/it/freedomotic/plugins/massabus/Massabus.java
@@ -1,7 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2013 Freedomotic team
+ * http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Freedomotic; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
+
 package it.freedomotic.plugins.massabus;
 
 import it.freedomotic.api.EventTemplate;
@@ -12,17 +30,18 @@ import it.freedomotic.reactions.Command;
 import it.freedomotic.serial.SerialConnectionProvider;
 import it.freedomotic.serial.SerialDataConsumer;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  *
  * @author enrico
  */
 public class Massabus extends Protocol implements SerialDataConsumer {
-
+    private static final Logger LOG = Logger.getLogger(Massabus.class.getName()); 
     SerialConnectionProvider serial;
 
     public Massabus() {
-        super("Massabus", "/it.massa.massabus/massabus-manifest.xml");
+        super("Massabus", "/massabus/massabus-manifest.xml");
         setPollingWait(2000); //waits 2000ms in onRun method before call onRun() again
     }
 
@@ -64,7 +83,7 @@ public class Massabus extends Protocol implements SerialDataConsumer {
             //written in setPollingWait() method [you can found it in Massabus constructor]
             String message = "A_STRING_MESSAGE";
             String reply = serial.send(message);
-            Freedomotic.logger.info("Massabus replies " + reply + " to message " + message);
+            LOG.info("Massabus replies " + reply + " to message " + message);
         } catch (IOException ex) {
             setDescription("Stopped for IOException in onRun"); //write here a better error message for the user
             stop();
@@ -83,7 +102,7 @@ public class Massabus extends Protocol implements SerialDataConsumer {
             setDescription("Stopped for IOException in onCommand"); //write here a better error message for the user
             stop();
         }
-        Freedomotic.logger.info("Massabus replies " + reply + " after executing command " + c.getName());
+        LOG.info("Massabus replies " + reply + " after executing command " + c.getName());
     }
 
     @Override
@@ -99,6 +118,6 @@ public class Massabus extends Protocol implements SerialDataConsumer {
     @Override
     public void onDataAvailable(String data) {
         //called when something is readed from the serial port
-        Freedomotic.logger.info("Massabus reads '" + data + "'");
+        LOG.info("Massabus reads '" + data + "'");
     }
 }
