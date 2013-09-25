298ab32d7da9017cde8f73a03f070ed10349b8d7
Matteo Mazzoni <matteo@bestmazzo.it>
2013-09-25 16:38:22 +0200

    PL-75: Allow PluginRemoteController plugin to start/stop/restart plugins  - added actions: START, STOP, RESTART  - added related privilege types

--- plugins/devices/test/src/main/java/it/freedomotic/plugins/PluginRemoteController.java
+++ plugins/devices/test/src/main/java/it/freedomotic/plugins/PluginRemoteController.java
@@ -1,34 +1,30 @@
 /**
  *
- * Copyright (c) 2009-2013 Freedomotic team
- * http://freedomotic.com
+ * Copyright (c) 2009-2013 Freedomotic team http://freedomotic.com
  *
  * This file is part of Freedomotic
  *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
  *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Freedomotic; see the file COPYING.  If not, see
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
 package it.freedomotic.plugins;
 
-import com.google.inject.Inject;
 import it.freedomotic.api.Actuator;
 import it.freedomotic.api.Client;
-import it.freedomotic.app.Freedomotic;
 import it.freedomotic.exceptions.UnableToExecuteException;
 import it.freedomotic.reactions.Command;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -38,8 +34,8 @@ import java.util.logging.Logger;
 public class PluginRemoteController
         extends Actuator {
 
-    private static final Logger LOG = Logger.getLogger(PluginRemoteController.class.getName()); 
-    
+    private static final Logger LOG = Logger.getLogger(PluginRemoteController.class.getName());
+
     public PluginRemoteController() {
         super("Plugins Remote Controller", "/test/plugins-remote-controller.xml");
     }
@@ -58,8 +54,25 @@ public class PluginRemoteController
             if (action.equalsIgnoreCase("HIDE")) {
                 plugin.hideGui();
             }
+            if (action.equalsIgnoreCase("STOP")) {
+                if (plugin != this && getApi().getAuth().isPermitted("sys:plugins:stop")) {
+                    plugin.stop();
+                }
+            }
+            if (action.equalsIgnoreCase("START")) {
+                if (plugin != this && getApi().getAuth().isPermitted("sys:plugins:start")) {
+                    plugin.start();
+                }
+            }
+            if (action.equalsIgnoreCase("RESTART")) {
+                if (plugin != this && getApi().getAuth().isPermitted("sys:plugins:stop,sys:plugins:start")) {
+                    plugin.stop();
+                    plugin.start();
+                }
+            }
+
         } else {
-            LOG.warning("Impossible to act on plugin " + c.getProperty("plugin"));
+            LOG.log(Level.WARNING, "Impossible to act on plugin {0}", c.getProperty("plugin"));
         }
     }
 
