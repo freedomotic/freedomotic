freedomotic/freedomotic

commit 0d9c08b556621efaad67b94f4fc4e36bf02d2a1c

PL-49 + PL-45 + some extras
 - adopted Apache OpenJPA
 - adapted data storage method
 - introduced extraction capability
 - created JSON marshalling of query data to be sent on message bus
 - embedded support for dbms: h2, mysql, sqlserver, sqlite
 - modified freedomotic-core/.../ReactionPersistence.java in order to avoid errors on concurrent accesses on a array
 - added localization on command/trigger auto generation
 - Created "Harvester Object" object plugin
 - added a Harvester Object into default environment
 - add some filters on data (time interval and time granularity)
 - TODO: add fields and params to change chart representation (e.g. points, bars, pie, etc.)
 - TODO: extend query and chart types, refine ui
 - TODO: complete data extraction for groups of objects (like objects with certain tags, in a specific room or environment ... )

framework/freedomotic-core/i18n/freedomotic.properties
@@ -88,3 +88,19 @@ area_floor=Area/Floor
 info=Info
 yes=Yes
 no=No
+when_X_is_clicked=When {0} is clicked
+when_X_turns_on=When {0} turns on
+when_X_turns_off=When {0} turns off
+set_X_brightness_to_50=Set {0} brightness to 50%
+increase_X_brightness=Increase {0} brightness
+decrease_X_brightness=Decrease {0} brightness
+set_its_brightness_to_50=Set its brightness to 50%
+increase_its_brightness=Increase its brightness
+decrease_its_brightness=Decrease its brightness
+set_brightness_from_event_value=Set its brightness to the value in the event
+turn_on_X=Turn on {0}
+turn_off_X=Turn off {0}
+switch_X_power=Switch {0} power
+turn_it_on=Turn_it_on
+turn_it_off=Turn_it_off
+switch_its_power=Switch its power

commit ba0cd7e2860d15dbac93920c19d5140aac0e7da6

Solved Core-345:  - Fixed a bug introduced in 0d9c08b55662 that resulted in wrong auto-generated trigger names  - Fixed reaction load, with attention to trigger mapping and other checks.

framework/freedomotic-core/i18n/freedomotic.properties
@@ -89,8 +89,8 @@ info=Info
 yes=Yes
 no=No
 when_X_is_clicked=When {0} is clicked
-when_X_turns_on=When {0} turns on
-when_X_turns_off=When {0} turns off
+X_turns_on={0} turns on
+X_turns_off={0} turns off
 set_X_brightness_to_50=Set {0} brightness to 50%
 increase_X_brightness=Increase {0} brightness
 decrease_X_brightness=Decrease {0} brightness
@@ -101,6 +101,6 @@ set_brightness_from_event_value=Set its brightness to the value in the event
 turn_on_X=Turn on {0}
 turn_off_X=Turn off {0}
 switch_X_power=Switch {0} power
-turn_it_on=Turn_it_on
-turn_it_off=Turn_it_off
+turn_it_on=Turn it on
+turn_it_off=Turn it off
 switch_its_power=Switch its power

framework/freedomotic-core/src/main/java/it/freedomotic/objects/impl/ElectricDevice.java
@@ -219,7 +219,7 @@ public class ElectricDevice
         clicked.setPersistence(false);
 
         Trigger turnsOn = new Trigger();
-        turnsOn.setName(I18n.msg("when_X_turns_on", new Object[]{this.getPojo().getName()}));
+        turnsOn.setName(I18n.msg("X_turns_on", new Object[]{this.getPojo().getName()}));
         turnsOn.setChannel("app.event.sensor.object.behavior.change");
         turnsOn.getPayload().addStatement("object.name",
                 this.getPojo().getName());
@@ -228,7 +228,7 @@ public class ElectricDevice
 //        XStream stream = FreedomXStream.getXstream();
 //        System.out.println(stream.toXML(turnsOn));
         Trigger turnsOff = new Trigger();
-        turnsOff.setName(I18n.msg("when_X_turns_off", new Object[]{this.getPojo().getName()}));
+        turnsOff.setName(I18n.msg("X_turns_off", new Object[]{this.getPojo().getName()}));
         turnsOff.setChannel("app.event.sensor.object.behavior.change");
         turnsOff.getPayload().addStatement("object.name",
                 this.getPojo().getName());

framework/freedomotic-core/src/main/java/it/freedomotic/reactions/CommandPersistence.java
@@ -58,10 +58,9 @@ public class CommandPersistence {
             if (!userCommands.containsKey(c.getName().trim().toLowerCase())) {
                 userCommands.put(c.getName(),
                         c);
-                LOG.fine("Added command '" + c.getName() + "' to the list of user commands");
+                LOG.log(Level.FINE, "Added command ''{0}'' to the list of user commands", c.getName());
             } else {
-                LOG.config("Command '" + c.getName()
-                        + "' already in the list of user commands. Skipped");
+                LOG.log(Level.CONFIG, "Command ''{0}'' already in the list of user commands. Skipped", c.getName());
             }
         } else {
             LOG.warning("Attempt to add a null user command to the list. Skipped");
@@ -101,11 +100,11 @@ public class CommandPersistence {
     }
 
     public static Command getHardwareCommand(String name) {
-        Command command = hardwareCommands.get(name);
+        Command command = hardwareCommands.get(name.trim());
 
         if (command == null) {
-            LOG.severe("Missing command '" + name + "'. "
-                    + "Maybe the related plugin is not installed or cannot be loaded");
+            LOG.log(Level.SEVERE,"Missing command ''{0}" + "''. "
+                    + "Maybe the related plugin is not installed or cannot be loaded", name);
         }
 
         return command;
@@ -138,10 +137,14 @@ public class CommandPersistence {
                 summary.append("#Filename \t\t #CommandName \t\t\t #Destination").append("\n");
 
                 for (File file : files) {
-                    String xml =
-                            DOMValidateDTD.validate(file, Info.getApplicationPath() + "/config/validator/command.dtd");
-                    Command command = null;
-
+                     Command command = null;
+                     String xml = null;
+                    try {
+                        xml = DOMValidateDTD.validate(file, Info.getApplicationPath() + "/config/validator/command.dtd");
+                     } catch (Exception e) {
+                        LOG.log(Level.SEVERE, "Reaction file {0} is not well formatted: {1}", new Object[]{file.getPath(), e.getLocalizedMessage()});
+                        continue;
+                    }
                     try {
                         command = (Command) xstream.fromXML(xml);
 
@@ -160,8 +163,7 @@ public class CommandPersistence {
                         summary.append(file.getName()).append("\t\t").append(command.getName())
                                 .append("\t\t\t").append(command.getReceiver()).append("\n");
                     } catch (CannotResolveClassException e) {
-                        LOG.severe("Cannot unserialize command due to unrecognized class '"
-                                + e.getMessage() + "' in \n" + xml);
+                        LOG.log(Level.SEVERE, "Cannot unserialize command due to unrecognized class ''{0}'' in \n{1}", new Object[]{e.getMessage(), xml});
                     }
                 }
 
@@ -181,21 +183,20 @@ public class CommandPersistence {
                 }
             }
         } else {
-            LOG.config("No commands to load from this folder " + folder.toString());
+            LOG.log(Level.CONFIG, "No commands to load from this folder {0}", folder.toString());
         }
     }
 
     public static void saveCommands(File folder) {
         try {
             if (userCommands.isEmpty()) {
-                LOG.warning("There are no commands to persist, " + folder.getAbsolutePath()
-                        + " will not be altered.");
+                LOG.log(Level.WARNING, "There are no commands to persist, {0} will not be altered.", folder.getAbsolutePath());
 
                 return;
             }
 
             if (!folder.isDirectory()) {
-                LOG.warning(folder.getAbsoluteFile() + " is not a valid command folder. Skipped");
+                LOG.log(Level.WARNING, "{0} is not a valid command folder. Skipped", folder.getAbsoluteFile());
 
                 return;
             }

framework/freedomotic-core/src/main/java/it/freedomotic/reactions/ReactionPersistence.java
@@ -1,22 +1,20 @@
 /**
  *
- * Copyright (c) 2009-2013 Freedomotic team
- * http://freedomotic.com
+ * Copyright (c) 2009-2013 Freedomotic team http://freedomotic.com
  *
  * This file is part of Freedomotic
  *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
  *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Freedomotic; see the file COPYING.  If not, see
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package it.freedomotic.reactions;
@@ -32,7 +30,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -40,6 +37,7 @@ import java.util.UUID;
 
 import com.thoughtworks.xstream.XStream;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -56,14 +54,13 @@ public class ReactionPersistence {
 
     public static void saveReactions(File folder) {
         if (list.isEmpty()) {
-            LOG.warning("There are no reactions to persist, " + folder.getAbsolutePath()
-                    + " will not be altered.");
+            LOG.log(Level.WARNING, "There are no reactions to persist, {0} will not be altered.", folder.getAbsolutePath());
 
             return;
         }
 
         if (!folder.isDirectory()) {
-            LOG.warning(folder.getAbsoluteFile() + " is not a valid reaction folder. Skipped");
+            LOG.log(Level.WARNING, "{0} is not a valid reaction folder. Skipped", folder.getAbsoluteFile());
 
             return;
         }
@@ -72,7 +69,7 @@ public class ReactionPersistence {
         deleteReactionFiles(folder);
 
         try {
-            LOG.config("Saving reactions to file in " + folder.getAbsolutePath());
+            LOG.log(Level.CONFIG, "Saving reactions to file in {0}", folder.getAbsolutePath());
 
             for (Reaction reaction : list) {
                 String uuid = reaction.getUUID();
@@ -142,25 +139,29 @@ public class ReactionPersistence {
 
             if (files != null) {
                 for (File file : files) {
-                    //validate the object against a predefined DTD
-                    String xml =
-                            DOMValidateDTD.validate(file, Info.getApplicationPath() + "/config/validator/reaction.dtd");
                     Reaction reaction = null;
-
+                    //validate the object against a predefined DTD
                     try {
+                        String xml =
+                                DOMValidateDTD.validate(file, Info.getApplicationPath() + "/config/validator/reaction.dtd");
+
                         reaction = (Reaction) xstream.fromXML(xml);
                     } catch (Exception e) {
-                        LOG.severe("Reaction file is not well formatted. \n" + Freedomotic.getStackTraceInfo(e));
-                       
+                        LOG.log(Level.SEVERE, "Reaction file {0} is not well formatted: {1}", new Object[]{file.getName(), e.getLocalizedMessage()});
                         continue;
                     }
 
-                    if (reaction.getCommands().size() == 0) {
-                        LOG.severe("Reaction " + reaction.toString()
-                                + " has no valid commands. Maybe related objects are missing or not configured properly.");
+                    if (reaction.getTrigger() !=  null && reaction.getTrigger().getName() != null) {
+                        add(reaction);
+                    } else {
+                        LOG.log(Level.SEVERE, "Cannot add reaction {0}: it has empty Trigger", file.getName());
+                        continue;
                     }
-
-                    add(reaction);
+                    
+                    if (reaction.getCommands().isEmpty()) {
+                        LOG.log(Level.WARNING, "Reaction {0} has no valid commands. Maybe related objects are missing or not configured properly.", reaction.toString());
+                    }
+                    
                     summary.append(reaction.getUUID()).append("\t\t\t").append(reaction.toString())
                             .append("\t\t\t").append(reaction.getDescription()).append("\n");
                 }
@@ -172,11 +173,10 @@ public class ReactionPersistence {
                 //Close the output stream
                 indexfile.close();
             } else {
-                LOG.config("No reactions to load from this folder " + folder.toString());
+                LOG.log(Level.CONFIG, "No reactions to load from this folder {0}", folder.toString());
             }
         } catch (Exception e) {
-            LOG.severe("Exception while loading reaction in " + folder.getAbsolutePath() + ".\n"
-                    + Freedomotic.getStackTraceInfo(e));
+            LOG.log(Level.SEVERE, "Exception while loading reaction in {0}.\n{1}", new Object[]{folder.getAbsolutePath(), Freedomotic.getStackTraceInfo(e)});
         }
     }
 
@@ -185,21 +185,21 @@ public class ReactionPersistence {
             r.getTrigger().register(); //trigger starts to listen on its channel
             list.add(r);
             r.setChanged();
-            LOG.config("Added new reaction " + r.getDescription());
+            LOG.log(Level.CONFIG, "Added new reaction {0}", r.getDescription());
+
         } else {
-            LOG.info("The reaction '" + r.getDescription() + "' is already loaded so its skipped.");
+            LOG.log(Level.INFO, "The reaction ''{0}'' is already loaded so it is skipped.", r.getDescription());
         }
     }
 
     public static void remove(Reaction input) {
         if (input != null) {
             boolean removed = list.remove(input);
-            LOG.info("Removed reaction " + input.getDescription());
+            LOG.log(Level.INFO, "Removed reaction {0}", input.getDescription());
             input.getTrigger().unregister();
 
             if ((!removed) && (list.contains(input))) {
-                LOG.warning("Error while removing Reaction " + input.getDescription()
-                        + " from the list");
+                LOG.log(Level.WARNING, "Error while removing Reaction {0} from the list", input.getDescription());
             }
         }
     }

framework/freedomotic-core/src/main/java/it/freedomotic/reactions/Trigger.java
@@ -82,7 +82,7 @@ public final class Trigger implements BusConsumer, Cloneable {
 	}
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name.trim();
     }
 
     public boolean isHardwareLevel() {

framework/freedomotic-core/src/main/java/it/freedomotic/reactions/TriggerPersistence.java
@@ -1,22 +1,20 @@
 /**
  *
- * Copyright (c) 2009-2013 Freedomotic team
- * http://freedomotic.com
+ * Copyright (c) 2009-2013 Freedomotic team http://freedomotic.com
  *
  * This file is part of Freedomotic
  *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
  *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Freedomotic; see the file COPYING.  If not, see
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package it.freedomotic.reactions;
@@ -41,6 +39,7 @@ import java.util.Iterator;
 import java.util.UUID;
 
 import com.thoughtworks.xstream.XStream;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -140,10 +139,16 @@ public class TriggerPersistence {
 
             if (files != null) {
                 for (File file : files) {
-                    //validate the object against a predefined DTD
-                    String xml =
-                            DOMValidateDTD.validate(file, Info.getApplicationPath() + "/config/validator/trigger.dtd");
-                    Trigger trigger = (Trigger) xstream.fromXML(xml);
+                    Trigger trigger = null;
+                    try {
+                        //validate the object against a predefined DTD
+                        String xml =
+                                DOMValidateDTD.validate(file, Info.getApplicationPath() + "/config/validator/trigger.dtd");
+                        trigger = (Trigger) xstream.fromXML(xml);
+                    } catch (Exception e) {
+                        LOG.log(Level.SEVERE, "Trigger file {0} is not well formatted: {1}", new Object[]{file.getPath(), e.getLocalizedMessage()});
+                        continue;
+                    }
 
                     //addAndRegister trigger to the list if it is not a duplicate
                     if (!list.contains(trigger)) {
@@ -203,21 +208,23 @@ public class TriggerPersistence {
     }
 
     public static synchronized void add(Trigger t) {
-        int preSize = TriggerPersistence.size();
+        if (t != null) {
+            int preSize = TriggerPersistence.size();
 
-        if (!list.contains(t)) {
-            list.add(t);
-        } else {
-            //this trigger is already in the list
-            int old = list.indexOf(t);
-            list.get(old).unregister();
-            list.set(old, t);
-        }
+            if (!list.contains(t)) {
+                list.add(t);
+            } else {
+                //this trigger is already in the list
+                int old = list.indexOf(t);
+                list.get(old).unregister();
+                list.set(old, t);
+            }
 
-        int postSize = TriggerPersistence.size();
+            int postSize = TriggerPersistence.size();
 
-        if (!(postSize == (preSize + 1))) {
-            LOG.severe("Error while while adding trigger '" + t.getName() + "'");
+            if (!(postSize == (preSize + 1))) {
+                LOG.severe("Error while while adding trigger '" + t.getName() + "'");
+            }
         }
     }
 
@@ -246,19 +253,17 @@ public class TriggerPersistence {
      * input
      */
     public static Trigger getTrigger(String name) {
-        if ((name == null) || (name.isEmpty())) {
+        if ((name == null) || (name.trim().isEmpty())) {
             return null;
         }
 
-        for (Iterator it = list.iterator(); it.hasNext();) {
-            Trigger trigger = (Trigger) it.next();
-
-            if (trigger.getName().equalsIgnoreCase(name)) {
+        for (Trigger trigger : list) {
+            if (trigger.getName().equalsIgnoreCase(name.trim())) {
                 return trigger;
             }
         }
 
-        LOG.warning("Searching for a trigger named '" + name + "' but it doesen't exist.");
+        LOG.log(Level.WARNING, "Searching for a trigger named ''{0}'' but it doesn''t exist.", name);
 
         return null;
     }