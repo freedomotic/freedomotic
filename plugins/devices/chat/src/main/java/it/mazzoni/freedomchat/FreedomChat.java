562212b310ce56c1600982f2e333ee211ba9021f
Matteo Mazzoni <matteo@bestmazzo.it>
2013-12-04 23:17:48 +0100

    Chat plugin: drafted support for Skype

--- plugins/devices/chat/src/main/java/it/mazzoni/freedomchat/FreedomChat.java
+++ plugins/devices/chat/src/main/java/it/mazzoni/freedomchat/FreedomChat.java
@@ -17,6 +17,10 @@
  */
 package it.mazzoni.freedomchat;
 
+import com.skype.ChatMessage;
+import com.skype.ChatMessageAdapter;
+import com.skype.Skype;
+import com.skype.SkypeException;
 import it.freedomotic.api.EventTemplate;
 import it.freedomotic.api.Protocol;
 import it.freedomotic.core.NaturalLanguageProcessor;
@@ -50,13 +54,17 @@ public class FreedomChat extends Protocol {
     private String password;
     private String acceptancePassword;
     private boolean tls;
+    private boolean useSkype;
+    private boolean useXMPP;
     Connection conn;
+    private ChatMessageAdapter chatListener;
     public static String IF = "if";
     public static String THEN = "then";
     public static String WHEN = "when";
     public static String HELP = "help";
     public static String LIST = "list";
-    
+    public static String ACCEPTED = "Just wait and see.";
+    public static String NOT_ACCEPTED = "";
 
     public FreedomChat() {
         //every plugin needs a name and a manifest XML file
@@ -67,85 +75,47 @@ public class FreedomChat extends Protocol {
         //POLLING_WAIT is the value of the property "time-between-reads" or 2000 millisecs,
         //default value if the property does not exist in the manifest
         setPollingWait(POLLING_WAIT); //millisecs interval between hardware device status reads
-
     }
 
     @Override
     protected void onStart() {
-        setDescription("Starting");
-        hostname = configuration.getStringProperty("hostname", "jabber.org");
-        port = configuration.getIntProperty("port", 5222);
-        username = configuration.getStringProperty("username", "");
-        password = configuration.getStringProperty("password", "");
-        acceptancePassword = configuration.getStringProperty("acceptance-password", "");
-        tls = configuration.getBooleanProperty("use-tls", false);
-
-        ConnectionConfiguration config = new ConnectionConfiguration(hostname, port);
-        config.setCompressionEnabled(true);
-        config.setSASLAuthenticationEnabled(tls);
-        config.setSelfSignedCertificateEnabled(true);
-
-        
-        conn = new XMPPConnection(config);
-        try {
-            conn.connect();
-            conn.login(username, password, "Freedomotic");
-        // Create a new presence. Pass in false to indicate we're unavailable.
-        Presence presence = new Presence(Presence.Type.available);
-        presence.setStatus("Ready");
-        // Send the packet (assume we have a Connection instance called "con").
-        conn.sendPacket(presence);
-
-        // wait for messages
-        ChatManager chatmanager = conn.getChatManager();
-        chatmanager.addChatListener(new ChatManagerListener() {
-            @Override
-            public void chatCreated(Chat chat, boolean createdLocally) {
-                if (!createdLocally) {
-                    chat.addMessageListener(new MessageListener() {
-                        @Override
-                        public void processMessage(Chat chat, Message message) {
-                            try {
-                                // the user is in my list, OK accepc messages
-                                LOG.info(chat.getParticipant());
-                                String uname[] = chat.getParticipant().split("/");
-                                if (conn.getRoster().getEntry(uname[0]) != null) {
-                                    // Send back the same text the other user sent us.
-                                    //chat.sendMessage(message.getBody());
-                                    chat.sendMessage(manageMessage(message.getBody()));
-
-                                } else {
-                                    // expect a password in order to add user to friends' list
-                                    chat.sendMessage(manageSubscription(chat, message));
-                                }
-
-                            } catch (XMPPException ex) {
-                                LOG.log(Level.SEVERE, null, ex);
-                            }
-                        }
-                    });
-                }
-            }
-        });
-
-        LOG.info("Chat plugin has started");
-        setDescription("Registered as: " + username + "\n Acceptance password: " + acceptancePassword);
-        
-        } catch (Exception ex) {
-            
-            LOG.log(Level.SEVERE, "Cannot start Chat plugin. Reason: {0}", ex.getMessage());
-            onStop();
+        setDescription("Chat plugin starting");
+        useSkype = configuration.getBooleanProperty("enable-skype", false);
+        useXMPP = configuration.getBooleanProperty("enable-XMPP", false);
+        if (!useSkype && !useXMPP) {
+            setDescription("Enable XMPP or Skype before starting");
+            return;
         }
-        
+        String newDescription = "";
+        acceptancePassword = configuration.getStringProperty("acceptance-password", "");
+        boolean skypeInited = false;
+        if (useSkype) {
+            skypeInited = initSkype();
+        }
+        boolean xmppInited = false;
+        if (useXMPP) {
+            xmppInited = initXMPP();
+            if (xmppInited) {
+                newDescription = "XMPP: " + username + ",accept. pwd: " + acceptancePassword;
+            }
+        }
+
+        if (xmppInited || skypeInited) {
+            if (skypeInited) {
+                setDescription("Skype enabled " + newDescription);
+            } else {
+                setDescription(newDescription);
+            }
+            LOG.info("Chat plugin has started");
+        } else {
+            setDescription("Chat plugin");
+        }
     }
 
     @Override
     protected void onStop() {
-        // Disconnect from the server
-        if (conn.isConnected()) {
-            conn.disconnect();
-        }
-        conn = null;
+        teardownXMPP();
+        teardownSkype();
         setDescription("Chat plugin");
         LOG.info("Chat plugin has stopped ");
     }
@@ -178,13 +148,13 @@ public class FreedomChat extends Protocol {
         String tokenMess[] = mess.split(" "); //nlp.getTokenizer().tokenize(sentenceMess[0]);
         String triggername = "";
         int conditionSep = 0;
-        if (tokenMess[0].equalsIgnoreCase(HELP)){
+        if (tokenMess[0].equalsIgnoreCase(HELP)) {
             return help(tokenMess);
         }
-        if (tokenMess[0].equalsIgnoreCase(LIST)){
+        if (tokenMess[0].equalsIgnoreCase(LIST)) {
             return list(tokenMess);
         }
-        
+
         if (tokenMess[0].equalsIgnoreCase(IF) || tokenMess[0].equalsIgnoreCase(WHEN)) {
             for (int i = 1; i < tokenMess.length; i++) {
                 if (tokenMess[i].equalsIgnoreCase(THEN)) {
@@ -199,10 +169,10 @@ public class FreedomChat extends Protocol {
         String commandName = unsplit(tokenMess, conditionSep, tokenMess.length - conditionSep, " ");
         List<NaturalLanguageProcessor.Rank> mostSimilar = nlp2.getMostSimilarCommand(commandName, 10);
         // user is asking for help
-        if (commandName.contains("*")){
-            String response ="";
-            for (NaturalLanguageProcessor.Rank nlpr : mostSimilar){
-                response += "? " +nlpr.getCommand().getName() + "\n";
+        if (commandName.contains("*")) {
+            String response = "";
+            for (NaturalLanguageProcessor.Rank nlpr : mostSimilar) {
+                response += "? " + nlpr.getCommand().getName() + "\n";
             }
             return response;
         }
@@ -225,18 +195,6 @@ public class FreedomChat extends Protocol {
             return c.getName() + "\n DONE.";
         }
         return "DONE";
-    }
-
-    private String manageSubscription(Chat chat, Message message) {
-        if (message.getBody().equals(acceptancePassword)) {
-            try {
-                conn.getRoster().createEntry(chat.getParticipant(), "", null);
-                return "Just wait and see.";
-            } catch (XMPPException ex) {
-                LOG.log(Level.SEVERE, null, ex);
-            }
-        }
-        return "";
     }
 
     public static String unsplit(String[] parts, int index, int length, String splitter) {
@@ -277,5 +235,135 @@ public class FreedomChat extends Protocol {
 
     private String list(String[] tokenMess) {
         return "Not yet implemented";
+    }
+
+    private boolean initXMPP() {
+        hostname = configuration.getStringProperty("hostname", "jabber.org");
+        port = configuration.getIntProperty("port", 5222);
+        username = configuration.getStringProperty("username", "");
+        password = configuration.getStringProperty("password", "");
+        tls = configuration.getBooleanProperty("use-tls", false);
+
+        ConnectionConfiguration config = new ConnectionConfiguration(hostname, port);
+        config.setCompressionEnabled(true);
+        config.setSASLAuthenticationEnabled(tls);
+        config.setSelfSignedCertificateEnabled(true);
+
+
+        conn = new XMPPConnection(config);
+        try {
+            conn.connect();
+            conn.login(username, password, "Freedomotic");
+            // Create a new presence. Pass in false to indicate we're unavailable.
+            Presence presence = new Presence(Presence.Type.available);
+            presence.setStatus("Ready");
+            // Send the packet (assume we have a Connection instance called "con").
+            conn.sendPacket(presence);
+
+            // wait for messages
+            ChatManager chatmanager = conn.getChatManager();
+            chatmanager.addChatListener(new ChatManagerListener() {
+                @Override
+                public void chatCreated(Chat chat, boolean createdLocally) {
+                    if (!createdLocally) {
+                        chat.addMessageListener(new MessageListener() {
+                            @Override
+                            public void processMessage(Chat chat, Message message) {
+                                try {
+                                    // the user is in my list, OK accepc messages
+                                    LOG.info(chat.getParticipant());
+                                    String uname[] = chat.getParticipant().split("/");
+                                    if (conn.getRoster().getEntry(uname[0]) != null) {
+                                        // Send back the same text the other user sent us.
+                                        //chat.sendMessage(message.getBody());
+                                        chat.sendMessage(manageMessage(message.getBody()));
+
+                                    } else {
+                                        // expect a password in order to add user to friends' list
+                                        chat.sendMessage(manageSubscription(chat, message));
+                                    }
+                                } catch (XMPPException ex) {
+                                    LOG.log(Level.SEVERE, null, ex);
+                                }
+                            }
+                        });
+                    }
+                }
+
+                private String manageSubscription(Chat chat, Message message) {
+                    if (message.getBody().equals(acceptancePassword)) {
+                        try {
+                            conn.getRoster().createEntry(chat.getParticipant(), "", null);
+                            return ACCEPTED;
+                        } catch (XMPPException ex) {
+                            LOG.log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    return NOT_ACCEPTED;
+                }
+            });
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Cannot start Chat plugin. Reason: {0}", ex.getMessage());
+            teardownXMPP();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean initSkype() {
+        Skype.setDaemon(false);
+        try {
+            chatListener = new ChatMessageAdapter() {
+                @Override
+                public void chatMessageReceived(ChatMessage received) {
+                    try {
+                        if (received.getSender().isAuthorized()) {
+                            if (received.getType().equals(ChatMessage.Type.SAID)) {
+                                received.getSender().send(manageMessage(received.getContent()));
+                            }
+                        } else {
+                            received.getSender().send(manageSubscription(received));
+                        }
+                    } catch (SkypeException ex) {
+                        LOG.log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                private String manageSubscription(ChatMessage received) {
+                    try {
+                        if (received.getContent().equals(acceptancePassword)) {
+                            received.getSender().setAuthorized(true);
+                            return ACCEPTED;
+                        }
+                    } catch (SkypeException ex) {
+                        LOG.log(Level.SEVERE, null, ex);
+                    }
+                    return NOT_ACCEPTED;
+                }
+            };
+            Skype.addChatMessageListener(chatListener);
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, ex.getMessage());
+            teardownSkype();
+            return false;
+        }
+        return true;
+    }
+
+    private void teardownXMPP() {
+        // Disconnect from the server
+        if (conn != null && conn.isConnected()) {
+            conn.disconnect();
+        }
+        conn = null;
+    }
+
+    private void teardownSkype() {
+        try {
+            Skype.removeChatMessageListener(chatListener);
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, ex.getMessage());
+        }
+        Skype.setDaemon(true);
     }
 }
