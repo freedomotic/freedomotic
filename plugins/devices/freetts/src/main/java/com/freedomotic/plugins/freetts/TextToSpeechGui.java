a7003c4a7f789b7accf2485dd138030581c4eaf2
Mauro Cicolella <mcicolella@libero.it>
2014-04-05 17:53:23 +0200

    Plugins: #Fixed PL-22. Mavenization of freetts plugin - Added missing dependencies - Updated copyright date - Disabled mbrola not working

--- plugins/devices/freetts/src/main/java/com/freedomotic/plugins/freetts/TextToSpeechGui.java
+++ plugins/devices/freetts/src/main/java/com/freedomotic/plugins/freetts/TextToSpeechGui.java
@@ -1,18 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2014 Freedomotic team http://freedomotic.com
+ * 
+* This file is part of Freedomotic
+ * 
+* This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ * 
+* This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+* You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
-/*
- * TextToSpeechGui.java
- *
- * Created on 9-dic-2009, 10.44.26
- */
-package com.freedomotic.plugins.tts;
+package com.freedomotic.plugins.freetts;
 
 /**
  *
- * @author Enrico
+ * @author Enrico Nicoletti
  */
 public class TextToSpeechGui extends javax.swing.JFrame {
 
