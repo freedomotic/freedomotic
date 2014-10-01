4e401ae9fe336fc3fd939c1d6dda3218b7198a2f
Mauro Cicolella <mcicolella@libero.it>
2014-10-01 15:52:50 +0200

    Fixed Core-201

--- framework/freedomotic-core/src/main/java/com/freedomotic/events/PersonEntersZone.java
+++ framework/freedomotic-core/src/main/java/com/freedomotic/events/PersonEntersZone.java
@@ -1,7 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2014 Freedomotic team http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.freedomotic.events;
 
@@ -9,6 +23,7 @@ import com.freedomotic.api.EventTemplate;
 import com.freedomotic.environment.ZoneLogic;
 import com.freedomotic.model.environment.Zone;
 import com.freedomotic.objects.impl.Person;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,6 +31,7 @@ import com.freedomotic.objects.impl.Person;
  */
 public final class PersonEntersZone extends EventTemplate {
 
+    private static final Logger LOG = Logger.getLogger(PersonEntersZone.class.getName());
     private final String uuid;
     private final String zoneName;
 
@@ -35,12 +51,21 @@ public final class PersonEntersZone extends EventTemplate {
     public String getDefaultDestination() {
         return "app.event.sensor.person.zone.enter";
     }
-    
+
     public String getPersonId() {
         return uuid;
     }
-    
+
     public String getZoneName() {
         return zoneName;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return ("Object " + uuid + " enters zone " + zoneName);
     }
 }
