0d508da8edcda7796c86e8cf88d0d87f19d70390
Mauro Cicolella <mcicolella@libero.it>
2014-10-06 16:39:00 +0200

    Fixed PL-136 - added missing header in jfrontend plugin source code - added 2 strings in i18n/freedomotic.properties

--- plugins/devices/frontend-java/src/main/java/com/freedomotic/jfrontend/PluginConfigure.java
+++ plugins/devices/frontend-java/src/main/java/com/freedomotic/jfrontend/PluginConfigure.java
@@ -1,6 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2014 Freedomotic team http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.freedomotic.jfrontend;
 
@@ -168,7 +183,7 @@ public class PluginConfigure
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle(api.getI18n().msg("plugins_configuration_editor"));
 
-        btnSave.setText(api.getI18n().msg("save"));
+        btnSave.setText(api.getI18n().msg("save_restart"));
         btnSave.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnSaveActionPerformed(evt);
@@ -184,7 +199,7 @@ public class PluginConfigure
 
         jScrollPane2.setViewportView(txtArea);
 
-        btnDefault.setText(api.getI18n().msg("restore_default"));
+        btnDefault.setText(api.getI18n().msg("undo_edit"));
         btnDefault.setEnabled(false);
         btnDefault.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
