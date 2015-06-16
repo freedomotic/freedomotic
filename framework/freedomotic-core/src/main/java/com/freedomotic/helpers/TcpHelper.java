8dae0da35f2da27be5ba551367ff419ffe0ff791
JMF <jmfidalgo@gmail.com>
2015-06-16 15:33:16 +0100

    Correct package declaration

--- framework/freedomotic-core/src/main/java/com/freedomotic/helpers/TcpHelper.java
+++ framework/freedomotic-core/src/main/java/com/freedomotic/helpers/TcpHelper.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.freedomotic.services;
+package com.freedomotic.helpers;
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
