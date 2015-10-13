e4183f84f77f749c8ab6882560f4c4d02c47a640
Mauro Cicolella <mcicolella@libero.it>
2015-10-13 16:23:10 +0200

    Fixed Core-526 - added some missing licence headers

--- framework/freedomotic-core/src/main/java/com/freedomotic/plugins/impl/PluginsManagerImpl.java
+++ framework/freedomotic-core/src/main/java/com/freedomotic/plugins/impl/PluginsManagerImpl.java
@@ -1,6 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2009-2015 Freedomotic team http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.freedomotic.plugins.impl;
 
@@ -30,6 +45,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -143,6 +159,7 @@ class PluginsManagerImpl implements PluginsManager {
 
     @Override
     public boolean installBoundle(URL fromURL) {
+                
         try {
             String url = fromURL.toString();
 
@@ -155,9 +172,11 @@ class PluginsManagerImpl implements PluginsManager {
             String filename = url.substring(url.lastIndexOf('/') + 1);
 
             //extracts plugin name from zip file name
-            String pluginName = filename.substring(0,
-                    filename.indexOf("-"));
-
+            String pluginNameRegex = "\\-\\d.\\d.";
+            Pattern p = Pattern.compile(pluginNameRegex);
+            String[] items = p.split(filename);
+            String pluginName = items[0];
+       
             //get the zip from the url and copy in plugin/device folder
             if (filename.endsWith(".device")) {
                 File zipFile = new File(Info.PATHS.PATH_DEVICES_FOLDER + "/" + filename);
