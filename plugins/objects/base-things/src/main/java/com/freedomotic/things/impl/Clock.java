fe3e5de847908883919e1af92a2a3899e3836530
Mauro Cicolella <mcicolella@libero.it>
2015-10-23 17:52:51 +0200

    Fixed #Core-547

--- plugins/objects/base-things/src/main/java/com/freedomotic/things/impl/Clock.java
+++ plugins/objects/base-things/src/main/java/com/freedomotic/things/impl/Clock.java
@@ -1,25 +1,22 @@
 /**
  *
- * Copyright (c) 2009-2015 Freedomotic team
- * http://freedomotic.com
+ * Copyright (c) 2009-2015 Freedomotic team http://freedomotic.com
  *
  * This file is part of Freedomotic
  *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
  *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Freedomotic; see the file COPYING.  If not, see
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
  * <http://www.gnu.org/licenses/>.
  */
-
 package com.freedomotic.things.impl;
 
 import com.freedomotic.events.ObjectReceiveClick;
@@ -30,7 +27,6 @@ import com.freedomotic.reactions.Trigger;
  *
  * @author enrico
  */
-
 public class Clock
         extends EnvObjectLogic {
 
@@ -53,24 +49,18 @@ public class Clock
         everySecond.setName("Every one second");
         everySecond.setDescription("schedule actions to be executed at a fixed interval of 1 second");
         everySecond.setChannel("app.event.sensor.calendar.event.schedule");
-        everySecond.getPayload().addStatement("object.name",
-                getPojo().getName());
         everySecond.setSuspensionTime(1000);
 
         Trigger everyMinute = new Trigger();
         everyMinute.setName("Every one minute");
         everyMinute.setDescription("schedule actions to be executed at a fixed interval of 60 second");
         everyMinute.setChannel("app.event.sensor.calendar.event.schedule");
-        everyMinute.getPayload().addStatement("object.name",
-                getPojo().getName());
         everyMinute.setSuspensionTime(60000);
 
         Trigger morning = new Trigger();
         morning.setName("Every minute while is morning");
-        morning.setDescription("executes a command every 60 second from 8:00 to 12:00");
+        morning.setDescription("execute a command every 60 second from 8:00 to 12:00");
         morning.setChannel("app.event.sensor.calendar.event.schedule");
-        morning.getPayload().addStatement("object.name",
-                getPojo().getName());
         morning.getPayload().addStatement("AND", "time.hour", "GREATER_THAN", "7");
         morning.getPayload().addStatement("AND", "time.hour", "LESS_THAN", "13");
         morning.setSuspensionTime(60000);
@@ -78,8 +68,7 @@ public class Clock
         Trigger clicked = new Trigger();
         clicked.setName("When " + this.getPojo().getName() + " is clicked");
         clicked.setChannel("app.event.sensor.object.behavior.clicked");
-        clicked.getPayload().addStatement("object.name",
-                this.getPojo().getName());
+        clicked.getPayload().addStatement("object.name", this.getPojo().getName());
         clicked.getPayload().addStatement("click", ObjectReceiveClick.SINGLE_CLICK);
         clicked.setPersistence(false);
 
@@ -87,8 +76,6 @@ public class Clock
         eight.setName("At 8:00AM");
         eight.setDescription("at 8:00 in the morning");
         eight.setChannel("app.event.sensor.calendar.event.schedule");
-        eight.getPayload().addStatement("object.name",
-                getPojo().getName());
         eight.getPayload().addStatement("AND", "time.hour", "EQUALS", "8");
         eight.getPayload().addStatement("AND", "time.minute", "EQUALS", "0");
         eight.getPayload().addStatement("AND", "time.second", "EQUALS", "0");
