a251259f7a02128d0e5468ed1a17a25607237d99
Mauro Cicolella <mcicolella@libero.it>
2016-11-18 18:00:39 +0100

    reduced initial delay for periodically saving

--- framework/freedomotic-core/src/main/java/com/freedomotic/util/PeriodicSave.java
+++ framework/freedomotic-core/src/main/java/com/freedomotic/util/PeriodicSave.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * Copyright (c) 2009-2016 Freedomotic team http://freedomotic.com
+ *
+ * This file is part of Freedomotic
+ *
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
+ *
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package com.freedomotic.util;
 
 import com.freedomotic.reactions.CommandRepository;
@@ -13,6 +32,7 @@ import java.util.concurrent.*;
  * @author https://github.com/wiewioraseb
  */
 public class PeriodicSave {
+
     private static final Logger LOG = LoggerFactory.getLogger(PeriodicSave.class.getName());
 
     private ScheduledExecutorService executorService;
@@ -22,21 +42,35 @@ public class PeriodicSave {
     private String savedDataRoot;
     private int executionInterval;
 
+    /**
+     *
+     * @param savedDataRoot
+     * @param executionInterval
+     */
     public PeriodicSave(String savedDataRoot, int executionInterval) {
         this.savedDataRoot = savedDataRoot;
         this.executionInterval = executionInterval;
     }
 
+    /**
+     *
+     * @param triggerRepository
+     * @param commandRepository
+     * @param reactionRepository
+     */
     public void delegateRepositories(TriggerRepository triggerRepository, CommandRepository commandRepository,
-                                     ReactionRepository reactionRepository) {
+            ReactionRepository reactionRepository) {
         this.triggerRepository = triggerRepository;
         this.commandRepository = commandRepository;
         this.reactionRepository = reactionRepository;
     }
 
+    /**
+     *
+     */
     public void startExecutorService() {
         executorService = Executors.newSingleThreadScheduledExecutor();
-        long initDelay = 15;
+        long initDelay = 5; //first saving after 5 minutes
         executorService.scheduleWithFixedDelay(runnable, initDelay, executionInterval, TimeUnit.MINUTES);
     }
 
@@ -49,6 +83,9 @@ public class PeriodicSave {
         }
     };
 
+    /**
+     *
+     */
     public void shutDown() {
         executorService.shutdown();
     }
