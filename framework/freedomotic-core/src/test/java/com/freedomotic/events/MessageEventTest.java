85d6a4e1a110eb0970130cd91f531a66fc42d764
upescatore <ubaldopescatore@gmail.com>
2017-10-02 22:06:19 +0200

    removed unused methods in MessageEventTest

--- framework/freedomotic-core/src/test/java/com/freedomotic/events/MessageEventTest.java
+++ framework/freedomotic-core/src/test/java/com/freedomotic/events/MessageEventTest.java
@@ -5,29 +5,9 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MessageEventTest {
-
-	@BeforeClass
-	public static void setUpClass() {
-	}
-
-	@AfterClass
-	public static void tearDownClass() {
-	}
-
-	@Before
-	public void setUp() {
-	}
-
-	@After
-	public void tearDown() {
-	}
 
 	@Test
 	public void messageEventCreation() {
