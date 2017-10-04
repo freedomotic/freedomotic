61f6f4aa9e8cb706a6e54ecfab040146def461c2
[a123920] Yann Irrilo <yann.irrilo@atos.net>
2017-10-04 23:44:12 +0200

    Code Gardening for AbstractConsumer Fixes freedomotic/freedomotic#324

--- framework/freedomotic-core/src/main/java/com/freedomotic/api/AbstractConsumer.java
+++ framework/freedomotic-core/src/main/java/com/freedomotic/api/AbstractConsumer.java
@@ -22,11 +22,15 @@ package com.freedomotic.api;
 import com.freedomotic.bus.BusConsumer;
 import com.freedomotic.bus.BusMessagesListener;
 import com.freedomotic.bus.BusService;
+import com.freedomotic.exceptions.FreedomoticRuntimeException;
 import com.freedomotic.exceptions.UnableToExecuteException;
 import com.freedomotic.reactions.Command;
+
 import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
@@ -39,116 +43,155 @@ import javax.jms.ObjectMessage;
  */
 public abstract class AbstractConsumer implements BusConsumer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AbstractConsumer.class.getName());
-    private final BusMessagesListener listener;
-    private final BusService busService;
-    private boolean automaticReply;
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractConsumer.class.getName());
+	private final BusMessagesListener listener;
+	private final BusService busService;
+	private boolean automaticReply;
 
-    /**
-     *
-     * @param c the command received
-     * @throws IOException
-     * @throws UnableToExecuteException
-     */
-    protected abstract void onCommand(Command c) throws IOException, UnableToExecuteException;
+	/**
+	 * Constructor of consumer.
+	 * 
+	 * @param busService	busService use by the consumer
+	 */
+	public AbstractConsumer(BusService busService) {
+		this.busService = busService;
+		listener = new BusMessagesListener(this, busService);
+		listener.consumeCommandFrom(getMessagingChannel());
+		setAutomaticReply(true);
+	}
 
-    /**
-     *
-     * @param event
-     */
-    protected abstract void onEvent(EventTemplate event);
+	/**
+	 * Makes actions implied by a command.
+	 * 
+	 * @param c	command to process
+	 * @throws IOException
+	 * @throws UnableToExecuteException
+	 */
+	protected abstract void onCommand(Command c) throws IOException, UnableToExecuteException;
 
-    protected abstract String getMessagingChannel();
+	/**
+	 * Makes actions implied by the event triggered.
+	 * 
+	 * @param event	event to process
+	 */
+	protected abstract void onEvent(EventTemplate event);
 
-    public AbstractConsumer(BusService busService) {
-        this.busService = busService;
-        listener = new BusMessagesListener(this, busService);
-        listener.consumeCommandFrom(getMessagingChannel());
-        setAutomaticReply(true);
-    }
+	/**
+	 * Gets the messaging channel.
+	 * 
+	 * @return String representing the messaging channel
+	 */
+	protected abstract String getMessagingChannel();
 
-    @Override
-    public void onMessage(ObjectMessage message) {
-        Object jmsObject;
-        try {
-            jmsObject = message.getObject();
+	/**
+	 * Handle a input message.
+	 * 
+	 * @param message	message to process.
+	 * @see com.freedomotic.bus.BusConsumer#onMessage(javax.jms.ObjectMessage)
+	 */
+	@Override
+	public void onMessage(ObjectMessage message) {
+		Object jmsObject;
+		try {
+			jmsObject = message.getObject();
 
-            if (jmsObject instanceof Command) {
-                final Command command = (Command) jmsObject;
-                LOG.info("\"{}\" receives command \"{}\" with parameters '{''{'{}'}''}'",
-                        new Object[]{
-                            this.getClass().getCanonicalName(),
-                            command.getName(),
-                            command.getProperties()});
-                ActuatorPerforms task = new ActuatorPerforms(command,
-                        message.getJMSReplyTo(),
-                        message.getJMSCorrelationID());
-                task.start();
-            } else {
-                if (jmsObject instanceof EventTemplate) {
-                    final EventTemplate event = (EventTemplate) jmsObject;
-                    onEvent(event);
-                } else {
-                    throw new RuntimeException("Unrecognized type in JMS message, is neither a Command or an Event");
-                }
-            }
-        } catch (JMSException ex) {
-            LOG.error("Error while receiving a JMS message", ex);
-        }
-    }
+			if (jmsObject instanceof Command) {
+				final Command command = (Command) jmsObject;
+				LOG.info("\"{}\" receives command \"{}\" with parameters '{''{'{}'}''}'",
+						this.getClass().getCanonicalName(), command.getName(), command.getProperties());
+				ActuatorPerforms task = new ActuatorPerforms(command, message.getJMSReplyTo(), message.getJMSCorrelationID());
+				task.start();
+			} else {
+				// TODO Maybe make an else if instead
+				if (jmsObject instanceof EventTemplate) {
+					final EventTemplate event = (EventTemplate) jmsObject;
+					onEvent(event);
+				} else {
+					throw new FreedomoticRuntimeException( "Unrecognized type in JMS message, is neither a Command or an Event");
+				}
+			}
+		} catch (JMSException ex) {
+			LOG.error("Error while receiving a JMS message", ex);
+		}
+	}
 
-    public boolean isAutomaticReply() {
-        return automaticReply;
-    }
+	/**
+	 * Informs whether the reply is automatic or not.
+	 * 
+	 * @return true if automatic and false instead
+	 */
+	public boolean isAutomaticReply() {
+		return automaticReply;
+	}
 
-    public void setAutomaticReply(boolean automaticReply) {
-        this.automaticReply = automaticReply;
-    }
+	/**
+	 * Set automaticReply flag.
+	 * 
+	 * @param automaticReply
+	 *            true if automatic, false instead
+	 */
+	public void setAutomaticReply(boolean automaticReply) {
+		this.automaticReply = automaticReply;
+	}
 
-    public BusService getBusService() {
-        return busService;
-    }
+	/**
+	 * Returns the used busService.
+	 * 
+	 * @return the used busService
+	 */
+	public BusService getBusService() {
+		return busService;
+	}
 
-    //public void reply(Command command) {
-    // sends back the command
-    //    final String defaultCorrelationID = "-1";
-    //    getBusService().reply(command, lastDestination, defaultCorrelationID);
-    //}
-    private class ActuatorPerforms extends Thread {
+	/**
+	 * TODO create a real brand new class instead of an inner class ?
+	 */
+	private class ActuatorPerforms extends Thread {
 
-        private final Command command;
-        private final Destination reply;
-        private final String correlationID;
+		private final Command command;
+		private final Destination reply;
+		private final String correlationID;
 
-        ActuatorPerforms(Command c, Destination reply, String correlationID) {
-            this.command = c;
-            this.reply = reply;
-            this.correlationID = correlationID;
-            this.setName("command-executor");
-        }
+		/**
+		 * Constructor of ActuatorPerforms.
+		 * 
+		 * @param c
+		 *            command to process
+		 * @param reply
+		 *            destination
+		 * @param correlationID
+		 *            identifier used for reply
+		 */
+		ActuatorPerforms(Command c, Destination reply, String correlationID) {
+			this.command = c;
+			this.reply = reply;
+			this.correlationID = correlationID;
+			this.setName("command-executor");
+		}
 
-        @Override
-        public void run() {
-            try {
-                // a command is supposed executed if the plugin doesn't say the contrary
-                command.setExecuted(true);
-                onCommand(command);
-            } catch (IOException ex) {
-                LOG.error(ex.getLocalizedMessage());
-                command.setExecuted(false);
-            } catch (UnableToExecuteException ex) {
-                command.setExecuted(false);
-                LOG.info("\"{}\" failed to execute command \"{}\": {}", new Object[]{getName(), command.getName(), ex.getMessage()});
-            }
+		@Override
+		public void run() {
+			try {
+				// a command is supposed executed if the plugin doesn't say the contrary
+				command.setExecuted(true);
+				onCommand(command);
+			} catch (IOException ex) {
+				LOG.error(ex.getLocalizedMessage());
+				command.setExecuted(false);
+			} catch (UnableToExecuteException ex) {
+				command.setExecuted(false);
+				LOG.info("\"{}\" failed to execute command \"{}\": {}", getName(), command.getName(), ex.getMessage());
+			}
 
-            // automatic-reply-to-command is used when the plugin executes the command in a
-            // separate thread. In this cases the onCommand() returns immediately (as execution is forked in a thread)
-            // and sometimes this is not the intended behavior. Take a look at the Delayer plugin configuration
-            // it has to call reply(...) explicitely
-            if ((isAutomaticReply()) && (command.getReplyTimeout() > 0)) {
-                getBusService().reply(command, reply, correlationID); //sends back the command marked as executed or not
-            }
-        }
-    }
+			// automatic-reply-to-command is used when the plugin executes the command in a
+			// separate thread. In this cases the onCommand() returns immediately (as execution is forked in a thread)
+			// and sometimes this is not the intended behavior. Take a look at the Delayer plugin configuration
+			// it has to call reply(...) explicitely
+			if ((isAutomaticReply()) && (command.getReplyTimeout() > 0)) {
+				// sends back the command marked as executed/ or not
+				getBusService().reply(command, reply, correlationID);
+			}
+		}
+	}
 
 }
