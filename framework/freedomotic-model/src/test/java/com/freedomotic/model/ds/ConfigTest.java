d3e8deea4a541b74eb27c234cf71fcc0460423e2
albertotn <12526457+albertotn@users.noreply.github.com>
2017-10-14 16:51:20 +0200

    improved Junit test

--- framework/freedomotic-model/src/test/java/com/freedomotic/model/ds/ConfigTest.java
+++ framework/freedomotic-model/src/test/java/com/freedomotic/model/ds/ConfigTest.java
@@ -19,13 +19,18 @@
  */
 package com.freedomotic.model.ds;
 
-import junit.framework.Assert;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- *
  * @author Enrico Nicoletti
  */
 public class ConfigTest {
@@ -33,7 +38,6 @@ public class ConfigTest {
     private static Config config;
 
     /**
-     *
      * @throws Exception
      */
     @BeforeClass
@@ -44,7 +48,6 @@ public class ConfigTest {
     }
 
     /**
-     *
      * @throws Exception
      */
     @AfterClass
@@ -53,42 +56,48 @@ public class ConfigTest {
 
     @Test
     public void testGetXmlFile() {
+    	config.setXmlFile("test.xml");
+    	assertTrue(config.getXmlFile().equals("test.xml"));
     }
 
     @Test
-    public void testSetXmlFile_File() {
+    public void testSetXmlFile_File() throws IOException {
+    	Path path = Files.createTempFile("freedomotic", "temp");
+    	config.setXmlFile(path.toFile());
+    	assertTrue(config.getXmlFile() !=null);
     }
 
     @Test
     public void testSetXmlFile_String() {
-    }
-
-    @Test
-    public void testSetProperty() {
-    }
-
-    @Test
-    public void testGetProperty() {
+    	config.setXmlFile("test.xml");
+    	assertTrue(config.getXmlFile().equals("test.xml"));
     }
 
     @Test
     public void testGetTuples() {
+    	assertTrue(config.getTuples()!=null);
     }
 
     @Test
     public void testEntrySet() {
+    	assertTrue(config.entrySet()!=null);
     }
 
     @Test
     public void testPut() {
+    	config.put("test", "000");
+    	assertTrue(config.getStringProperty("test", null)!=null);
     }
 
     @Test
     public void testGetStringProperty() {
+    	assertTrue(config.getStringProperty("boolean-property-true", null)!=null);
     }
 
     @Test
     public void testGetIntProperty() {
+    	config.put("testInt", "1");
+    	assertTrue(config.getIntProperty("testInt",0) == 1);
     }
 
     /**
@@ -96,8 +105,8 @@ public class ConfigTest {
      */
     @Test
     public void testGetBooleanProperty() {
-        Assert.assertTrue("This property in Config must be true", config.getBooleanProperty("boolean-property-true", false));
-        Assert.assertFalse("This property in Config must be false", config.getBooleanProperty("boolean-property-false", true));
+        assertTrue("This property in Config must be true", config.getBooleanProperty("boolean-property-true", false));
+        assertFalse("This property in Config must be false", config.getBooleanProperty("boolean-property-false", true));
     }
 
     @Test
