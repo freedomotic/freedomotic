95f30e3308300421a564b2e33b97b02d5802239c
sahal.sahal <sahal.saad@servicerocket.com>
2017-10-17 09:21:01 +0800

    #371 Code gardening for PluginJList.

--- plugins/devices/frontend-java/src/main/java/com/freedomotic/jfrontend/PluginJList.java
+++ plugins/devices/frontend-java/src/main/java/com/freedomotic/jfrontend/PluginJList.java
@@ -22,7 +22,6 @@ package com.freedomotic.jfrontend;
 import com.freedomotic.api.API;
 import com.freedomotic.api.Client;
 import com.freedomotic.app.Freedomotic;
-import com.freedomotic.core.ResourcesManager;
 import com.freedomotic.plugins.ObjectPluginPlaceholder;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -30,12 +29,12 @@ import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.Vector;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -44,8 +43,12 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.ListCellRenderer;
+
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.freedomotic.core.ResourcesManager.getResource;
 
 /**
  *
@@ -54,26 +57,33 @@ import org.slf4j.LoggerFactory;
 public final class PluginJList extends JList {
 
     private static final Logger LOG = LoggerFactory.getLogger(PluginJList.class.getName());
+    private static final String PLUGIN_TYPE = "plugin";
+    private static final String OBJECT_TYPE = "object";
     private String filter;
 
-    /**
-     *
-     */
-    public boolean inDrag = false;
+    private MainWindow mainWindow;
+
+    public enum Icon {
+        RUNNING("running"),
+        STOPPED("stopped");
+
+        private String type;
+
+        Icon(String type) {
+            this.type = type;
+        }
+
+        public String getValue() {
+            return type;
+        }
+    }
 
     /**
-     *
+     * @param mainWindow
      */
-    public int dragged = 0;
-    private MainWindow parent;
-
-    /**
-     *
-     * @param parent
-     */
-    public PluginJList(final MainWindow parent) {
-        this.parent = parent;
-        setFilter("plugin"); //default value for filterning the list
+    PluginJList(final MainWindow mainWindow) {
+        this.mainWindow = mainWindow;
+        setFilter(PLUGIN_TYPE); //default value for filtering the list
         addMouseListener(new MouseAdapter() {
 
             @Override
@@ -83,88 +93,71 @@ public final class PluginJList extends JList {
                 java.util.List<Client> clients = (java.util.List<Client>) getApi().getClients(getFilter());
                 Client client = clients.get(i);
 
-                if (e.getButton() == MouseEvent.BUTTON1) {
-                    if (e.getClickCount() == 2) {
-                        if (client.isRunning()) {
-                            client.stop();
-                        } else {
-                            client.start();
-                        }
-
-                        update();
-                    }
-                }
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                if (e.isPopupTrigger()) {
-                    doPop(e);
-                } else {
-                    //drag started
-                    inDrag = true;
-                    dragged = 0;
-                }
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                if (e.isPopupTrigger()) {
-                    doPop(e);
-                }
-            }
-
-            private void doPop(MouseEvent e) {
-                Point p = e.getPoint();
-                int i = locationToIndex(p);
-                java.util.List<Client> clients = (java.util.List<Client>) getApi().getClients(getFilter());
-                final Client client = clients.get(i);
-                JPopupMenu menu = new JPopupMenu();
-                JMenuItem mnuConfigure = null;
-
-                if (client.getType().equalsIgnoreCase("plugin")) {
-                    mnuConfigure = new JMenuItem("Configure " + client.getName());
-                } else {
-                    if ("object".equalsIgnoreCase(client.getType())) {
-                        mnuConfigure = new JMenuItem("Add " + client.getName() + " Object");
+                if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 2) {
+                    if (client.isRunning()) {
+                        client.stop();
                     } else {
-                        mnuConfigure = new JMenuItem("Placeholder menu");
+                        client.start();
                     }
+
+                    update();
                 }
+            }
 
-                mnuConfigure.addActionListener(new ActionListener() {
+            @Override
+            public void mousePressed(MouseEvent event) {
+                if (event.isPopupTrigger()) {
+                    doPop(event);
+                }
+            }
 
-                    public void actionPerformed(ActionEvent e) {
-                        if (client.getType().equalsIgnoreCase("plugin")) {
-                            client.start();
-                            client.showGui();
-                            update();
-                        }
-
-                        if (client.getType().equalsIgnoreCase("object")) {
-                            ObjectPluginPlaceholder objp = (ObjectPluginPlaceholder) client;
-
-                            if (parent instanceof MainWindow) {
-                                MainWindow mw = parent;
-                                objp.startOnEnv(mw.getDrawer().getCurrEnv()); //adds the object to the environment
-                            }
-                        }
-                    }
-                });
-                menu.add(mnuConfigure);
-                menu.show(e.getComponent(),
-                        e.getX(),
-                        e.getY());
+            @Override
+            public void mouseReleased(MouseEvent event) {
+                if (event.isPopupTrigger()) {
+                    doPop(event);
+                }
             }
         });
     }
 
+    private void doPop(MouseEvent event) {
+        Point point = event.getPoint();
+        int index = locationToIndex(point);
+        List<Client> clients = Lists.newArrayList(getApi().getClients(getFilter()));
+        final Client client = clients.get(index);
+        JPopupMenu menu = new JPopupMenu();
+        JMenuItem mnuConfigure;
+
+        if (PLUGIN_TYPE.equalsIgnoreCase(client.getType())) {
+            mnuConfigure = new JMenuItem("Configure " + client.getName());
+        } else if (OBJECT_TYPE.equalsIgnoreCase(client.getType())) {
+            mnuConfigure = new JMenuItem("Add " + client.getName() + " Object");
+        } else {
+            mnuConfigure = new JMenuItem("Placeholder menu");
+        }
+
+
+        mnuConfigure.addActionListener(e1 -> {
+            if (PLUGIN_TYPE.equalsIgnoreCase(client.getType())) {
+                client.start();
+                client.showGui();
+                update();
+            } else if (OBJECT_TYPE.equalsIgnoreCase(client.getType())) {
+                ObjectPluginPlaceholder pluginPlaceholder = (ObjectPluginPlaceholder) client;
+                if (mainWindow != null) {
+                    pluginPlaceholder.startOnEnv(mainWindow.getDrawer().getCurrEnv()); //adds the object to the environment
+                }
+            }
+        });
+        menu.add(mnuConfigure);
+        menu.show(event.getComponent(), event.getX(), event.getY());
+    }
+
     private API getApi() {
-        return parent.getPlugin().getApi();
+        return mainWindow.getPlugin().getApi();
     }
 
     /**
-     *
      * @return
      */
     public String getFilter() {
@@ -172,7 +165,6 @@ public final class PluginJList extends JList {
     }
 
     /**
-     *
      * @param filter
      */
     public void setFilter(String filter) {
@@ -185,94 +177,16 @@ public final class PluginJList extends JList {
      */
     public void update() {
         try {
-            ImageIcon defaultIconRunning
-                    = new ImageIcon(ResourcesManager.getResource("plugin-running.png", 64, 64)); //new ImageIcon(path + File.separatorChar + "plug.png", "Icon");
-            ImageIcon defaultIconStopped
-                    = new ImageIcon(ResourcesManager.getResource("plugin-stopped.png", 64, 64)); //new ImageIcon(path + File.separatorChar + "plug-cool.png", "Icon");
 
             Vector vector = new Vector();
             Collection<Client> clients = getApi().getClients(getFilter());
 
-            for (Client addon : clients) {
-                if (addon.getType().equalsIgnoreCase(getFilter())) {
-                    boolean isRunning = addon.isRunning();
-                    JPanel jp = new JPanel();
-
-                    jp.setLayout(new BorderLayout());
-
-                    BufferedImage imageRunning = null;
-                    BufferedImage imageStopped = null;
-
-                    if (addon.getType().equalsIgnoreCase("plugin")) {
-                        imageRunning = ResourcesManager.getResource(addon.getClass().getSimpleName().toLowerCase()
-                                + "-running.png", 64, 64);
-                        imageStopped = ResourcesManager.getResource(addon.getClass().getSimpleName().toLowerCase()
-                                + "-stopped.png", 64, 64);
-                    } else {
-                        if (addon.getType().equalsIgnoreCase("object")) {
-                            ObjectPluginPlaceholder obj = (ObjectPluginPlaceholder) addon;
-                            String icon = obj.getObject().getPojo().getRepresentations().get(0).getIcon();
-
-                            if (icon != null) {
-                                imageRunning = ResourcesManager.getResource(icon, 64, 64);
-                                imageStopped = ResourcesManager.getResource(icon, 64, 64);
-                            }
-                        }
-                    }
-
-                    ImageIcon customIconRunning = defaultIconRunning;
-                    ImageIcon customIconStopped = defaultIconStopped;
-
-                    if (imageRunning != null) {
-                        customIconRunning = new ImageIcon(imageRunning);
-                    }
-
-                    if (imageStopped != null) {
-                        customIconStopped = new ImageIcon(imageStopped);
-                    }
-
-                    if (isRunning) {
-                        jp.add(new JLabel(customIconRunning),
-                                BorderLayout.LINE_START);
-                    } else {
-                        jp.add(new JLabel(customIconStopped),
-                                BorderLayout.LINE_START);
-                    }
-
-                    JLabel text = new JLabel(addon.getName());
-                    String description = addon.getDescription();
-
-                    JLabel lblDescription = new JLabel(description);
-                    text.setForeground(Color.black);
-
-                    Font font = getFont();
-                    text.setFont(font.deriveFont(Font.BOLD, 12));
-                    lblDescription.setForeground(Color.gray);
-
-                    if (!isRunning) {
-                        text.setForeground(Color.lightGray);
-                        lblDescription.setForeground(Color.lightGray);
-                    }
-
-                    JPanel jpcenter = new JPanel();
-                    GridLayout grid = new GridLayout(0, 1);
-                    jpcenter.setLayout(grid);
-                    jpcenter.setOpaque(false);
-                    jp.add(jpcenter, BorderLayout.CENTER);
-                    jpcenter.add(text);
-                    jpcenter.add(lblDescription);
-                    jpcenter.setToolTipText(description);
-
-                    JPanel last = new JPanel();
-                    last.setOpaque(false);
-                    jp.add(last, BorderLayout.LINE_END);
-                    jp.setBackground(Color.white);
-                    vector.add(jp);
-                }
-            }
+            clients.stream()
+                    .filter(client -> client.getType().equalsIgnoreCase(getFilter()))
+                    .map(this::createJPanelForClient)
+                    .forEach(vector::add);
 
             setListData(vector);
-
             ListCellRenderer renderer = new CustomCellRenderer();
             setCellRenderer(renderer);
         } catch (Exception e) {
@@ -280,23 +194,92 @@ public final class PluginJList extends JList {
         }
     }
 
-    class CustomCellRenderer
-            implements ListCellRenderer {
+    private JPanel createJPanelForClient(Client client) {
+        boolean isRunning = client.isRunning();
+        JPanel jPanelMain = new JPanel();
+        jPanelMain.setLayout(new BorderLayout());
+
+        if (isRunning) {
+            jPanelMain.add(new JLabel(getImageIcon(client, Icon.RUNNING.getValue())), BorderLayout.LINE_START);
+        } else {
+            jPanelMain.add(new JLabel(getImageIcon(client, Icon.STOPPED.getValue())), BorderLayout.LINE_START);
+        }
+
+        JLabel text = new JLabel(client.getName());
+        String description = client.getDescription();
+
+        JLabel lblDescription = new JLabel(description);
+        text.setForeground(Color.black);
+
+        Font font = getFont();
+        text.setFont(font.deriveFont(Font.BOLD, 12));
+        lblDescription.setForeground(Color.gray);
+
+        if (!isRunning) {
+            text.setForeground(Color.lightGray);
+            lblDescription.setForeground(Color.lightGray);
+        }
+
+        JPanel jPanelCenter = new JPanel();
+        GridLayout grid = new GridLayout(0, 1);
+        jPanelCenter.setLayout(grid);
+        jPanelCenter.setOpaque(false);
+        jPanelCenter.add(text);
+        jPanelCenter.add(lblDescription);
+        jPanelCenter.setToolTipText(description);
+
+        JPanel jPanelLast = new JPanel();
+        jPanelLast.setOpaque(false);
+
+        jPanelMain.add(jPanelCenter, BorderLayout.CENTER);
+        jPanelMain.add(jPanelLast, BorderLayout.LINE_END);
+        jPanelMain.setBackground(Color.white);
+
+        return jPanelMain;
+    }
+
+    private ImageIcon getImageIcon(Client client, String iconType) {
+        ImageIcon defaultIcon = new ImageIcon(getResource("plugin-" + iconType + ".png", 64, 64));
+
+        Optional<BufferedImage> image = Optional.empty();
+        if (PLUGIN_TYPE.equalsIgnoreCase(client.getType())) {
+            image = getClientImage(client.getClass().getSimpleName().toLowerCase() + "-" + iconType + ".png");
+        } else if (OBJECT_TYPE.equalsIgnoreCase(client.getType())) {
+            image = getPlaceHolderImage(client);
+        }
+
+        return image.map(ImageIcon::new).orElse(defaultIcon);
+    }
+
+    private Optional<BufferedImage> getClientImage(String imageName) {
+        return Optional.ofNullable(getResource(imageName, 64, 64));
+    }
+
+    private Optional<BufferedImage> getPlaceHolderImage(Client client) {
+        ObjectPluginPlaceholder obj = (ObjectPluginPlaceholder) client;
+        String icon = obj.getObject().getPojo().getRepresentations().get(0).getIcon();
+
+        if (icon == null) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(getResource(icon, 64, 64));
+    }
+
+    class CustomCellRenderer implements ListCellRenderer {
 
         @Override
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
-                boolean cellHasFocus) {
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             Component component = (Component) value;
-            component.setBackground(isSelected ? list.getSelectionBackground()
-                    : getListBackground(list, value, index, isSelected, cellHasFocus));
+            component.setBackground(isSelected ? list.getSelectionBackground() : getListBackground(list, index));
             component.setForeground(isSelected ? list.getSelectionForeground() : list.getForeground());
 
             return component;
         }
 
-        private Color getListBackground(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        private Color getListBackground(JList list, int index) {
             if ((index % 2) == 0) {
-                return (Color.decode("#f7f7f7"));
+                return Color.decode("#f7f7f7");
             } else {
                 return list.getBackground();
             }
