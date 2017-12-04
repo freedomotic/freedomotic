c7af221bfb79358be067e111ba98f313943b72f5
Mauro Cicolella <mcicolella@libero.it>
2017-12-04 15:34:51 +0100

    refactoring of hwg-ste plugin

--- plugins/devices/hwg-ste/src/main/java/com/freedomotic/plugins/devices/hwgste/Board.java
+++ plugins/devices/hwg-ste/src/main/java/com/freedomotic/plugins/devices/hwgste/Board.java
@@ -1,40 +1,51 @@
 /**
  *
- * Copyright (c) 2009-2016 Freedomotic team
- * http://freedomotic.com
+ * Copyright (c) 2009-2016 Freedomotic team http://freedomotic.com
  *
  * This file is part of Freedomotic
  *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
+ * This Program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2, or (at your option) any later version.
  *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This Program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Freedomotic; see the file COPYING.  If not, see
+ * You should have received a copy of the GNU General Public License along with
+ * Freedomotic; see the file COPYING. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
-
 package com.freedomotic.plugins.devices.hwgste;
+
+/**
+ * @autor Mauro Cicolella
+ */
 
 public class Board {
 
-    private String ipAddress = null;
+    private String alias;
+    private String ipAddress;
     private int snmpPort;
     private int port;
     private int sensorsNumber;
 
-    public Board(String ipAddress, int port, int sensorsNumber) {
+    public Board(String alias, String ipAddress, int port, int sensorsNumber) {
+        setAlias(alias);
         setIpAddress(ipAddress);
         setPort(port);
         setSensorsNumber(sensorsNumber);
 
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
     }
 
     public String getIpAddress() {
@@ -53,8 +64,8 @@ public class Board {
         this.port = port;
     }
 
-    public void setSnmpPort(int port) {
-        this.snmpPort = port;
+    public void setSnmpPort(int snmpPort) {
+        this.snmpPort = snmpPort;
     }
 
     public int getSensorsNumber() {
