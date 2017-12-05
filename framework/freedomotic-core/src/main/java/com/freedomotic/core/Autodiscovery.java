4be73595df29bea3eea51c653369543743c0e65d
Mauro Cicolella <mcicolella@libero.it>
2017-12-05 15:39:52 +0100

    Fixed #419 Autodiscovering: add a new thing to a fixed position

--- framework/freedomotic-core/src/main/java/com/freedomotic/core/Autodiscovery.java
+++ framework/freedomotic-core/src/main/java/com/freedomotic/core/Autodiscovery.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (c) 2009-2014 Freedomotic team http://freedomotic.com
+ * Copyright (c) 2009-2017 Freedomotic team http://freedomotic.com
  *
  * This file is part of Freedomotic
  *
@@ -54,221 +54,221 @@ import com.google.inject.Inject;
  */
 public final class Autodiscovery extends AbstractConsumer {
 
-	/**
-	 * Message queue channel name
-	 */
-	private static final String MESSAGING_CHANNEL = "app.objects.create";
+    /**
+     * Message queue channel name
+     */
+    private static final String MESSAGING_CHANNEL = "app.objects.create";
 
     /**
      * Class logger
      */
-	private static final Logger LOG = LoggerFactory.getLogger(Autodiscovery.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(Autodiscovery.class.getName());
 
-	// Dependencies
-	private final ClientStorage clientStorage;
-	private final ThingRepository thingsRepository;
-	private final TriggerRepository triggerRepository;
-	private final CommandRepository commandRepository;
+    // Dependencies
+    private final ClientStorage clientStorage;
+    private final ThingRepository thingsRepository;
+    private final TriggerRepository triggerRepository;
+    private final CommandRepository commandRepository;
 
-	@Inject
-	Autodiscovery(ClientStorage clientStorage, ThingRepository thingsRepository, TriggerRepository triggerRepository,
-			CommandRepository commandRepository, BusService busService) {
-		super(busService);
-		this.clientStorage = clientStorage;
-		this.thingsRepository = thingsRepository;
-		this.triggerRepository = triggerRepository;
-		this.commandRepository = commandRepository;
-	}
+    @Inject
+    Autodiscovery(ClientStorage clientStorage, ThingRepository thingsRepository, TriggerRepository triggerRepository,
+            CommandRepository commandRepository, BusService busService) {
+        super(busService);
+        this.clientStorage = clientStorage;
+        this.thingsRepository = thingsRepository;
+        this.triggerRepository = triggerRepository;
+        this.commandRepository = commandRepository;
+    }
 
-	/**
-	 * Gets {@link #MESSAGING_CHANNEL} string constant
-	 * @return messaging channel string constant
-	 */
-	@Override
-	public String getMessagingChannel() {
-		return MESSAGING_CHANNEL;
-	}
+    /**
+     * Gets {@link #MESSAGING_CHANNEL} string constant
+     *
+     * @return messaging channel string constant
+     */
+    @Override
+    public String getMessagingChannel() {
+        return MESSAGING_CHANNEL;
+    }
 
     /**
      * Gets properties from command parameter and calls {@link #join}
-     * @param command contains needed properties for parameters to call <code>join</code>
-     *                method
+     *
+     * @param command contains needed properties for parameters to call
+     * <code>join</code> method
      */
-	@Override
-	protected void onCommand(Command command) {
-		String name = command.getProperty("object.name");
-		String protocol = command.getProperty("object.protocol");
-		String address = command.getProperty("object.address");
-		String clazz = command.getProperty("object.class");
-		// Creates a Thing also if one with the same name exists, true by
-		// default
-		boolean allowClones = command.getBooleanProperty("autodiscovery.allow-clones", true);
+    @Override
+    protected void onCommand(Command command) {
+        String name = command.getProperty("object.name");
+        String protocol = command.getProperty("object.protocol");
+        String address = command.getProperty("object.address");
+        String clazz = command.getProperty("object.class");
+        // Creates a Thing also if one with the same name exists, true by
+        // default
+        boolean allowClones = command.getBooleanProperty("autodiscovery.allow-clones", true);
 
-		try {
-			join(clazz, name, protocol, address, allowClones);
-		} catch (RepositoryException ex) {
-			LOG.error(Freedomotic.getStackTraceInfo(ex));
-		}
-	}
-
-	/**
-	 * Creates an {@link EnvObjectLogic} with the specification in input. If an
-	 * {@link EnvObjectLogic} with the same protocol and address already exists
-	 * it will exit with no changes.
-	 *
-	 * @param clazz
-	 *            the name of the thing template to load (eg: Light)
-	 * @param name
-	 *            the name of the thing
-	 * @param protocol
-	 *            the protocol which drives the thing
-	 * @param address
-	 *            the address to uniquely identify the thing
-	 * @param allowClones
-	 *            determines if an existent template can be cloned by adding an
-	 *            ordinal number to its name, true by default
-	 * @return the thing created
-	 * @throws com.freedomotic.exceptions.RepositoryException
-	 *             if it is not possible to retrieve the requested information
-	 */
-	protected EnvObjectLogic join(String clazz, String name, String protocol, String address, boolean allowClones)
-			throws RepositoryException {
-		// Check if autodiscovery can be applied
-		if (thingAlreadyExists(protocol, address)) {
-			LOG.info(
-					"A thing with protocol \"{}\" and address \"{}\" already exists in the environment. Autodiscovery exits without changes",
-					protocol, address);
-			return null;
-		}
-
-		// If not allowed to clone an Thing
-		if (!allowClones && thingAlreadyExists(name)) {
-			LOG.info(
-					"A thing with name \"{}\" already exists in the environment. Autodiscovery exits without changes because property 'autodiscovery.allow-clones' property is ''{}'' for the received command",
-					name, false);
-			return null;
-		}
-
-		// Check if the requested Thing template is loaded
-		ObjectPluginPlaceholder thingTemplate = (ObjectPluginPlaceholder) clientStorage.get(clazz);
-		if (thingTemplate == null) {
-			LOG.warn("Autodiscovery error: doesn't exist an object class called \"{}\"", clazz);
-			return null;
-		}
-
-		// Start the new Thing creation
-		LOG.warn("Autodiscovery request for an object called \"{}\" of type \"{}\"", name, clazz);
-		File templateFile = thingTemplate.getTemplate();
-		EnvObjectLogic loaded = thingsRepository.load(templateFile);
-
-		// changing the name and other properties invalidates related trigger
-		// and commands
-		// call init() again after this changes
-		if ((name != null) && !name.isEmpty()) {
-			loaded.getPojo().setName(name);
-		} else {
-			loaded.getPojo().setName(protocol);
-		}
-
-		loaded = thingsRepository.copy(loaded);
-		loaded.getPojo().setProtocol(protocol);
-		loaded.getPojo().setPhisicalAddress(address);
-		// Remove the 'virtual' tag and any other actAs configuration.
-		// TODO: it would be better to remove the actAs property and manage all
-		// with tags
-		loaded.getPojo().setActAs("");
-		loaded.setRandomLocation();
-		configureOptionalMapping(protocol, clazz, loaded);
-		LOG.info("Autodiscovery adds a thing called \"{}\" of type \"{}\"", loaded.getPojo().getName(), clazz);
-		return loaded;
-	}
-
-	/**
-	 * Sets the PREFERRED MAPPING of the protocol plugin, if any is defined in
-	 * its manifest. The mapping information will be asked to the plugin which
-	 * implements the same protocol as the one specified in the arguments of
-	 * this method.
-	 * <p>
-	 * The clazz parameter is used to identify a tuple in the plugin manifest.
-	 * The configuration specified in this tuple will be applied to the target
-	 * Thing.
-	 *
-	 * @param protocol
-	 *            the thing protocol, used to retrieve the corresponding plugin
-	 * @param clazz
-	 *            the dot notation taxonomy used to identify the mapping that
-	 *            should be applied (eg: EnvObject.ElectricDevice.Light)
-	 * @param loaded
-	 *            the target things
-	 */
-	private void configureOptionalMapping(String protocol, String clazz, EnvObjectLogic loaded) {
-		Client addon = clientStorage.getClientByProtocol(protocol);
-
-		if ((addon != null) && (addon.getConfiguration().getTuples() != null)) {
-			for (int i = 0; i < addon.getConfiguration().getTuples().size(); i++) {
-				Map tuple = addon.getConfiguration().getTuples().getTuple(i);
-				String regex = (String) tuple.get("object.class");
-
-				if ((regex != null) && clazz.matches(regex)) {
-					this.mapBehaviorsToHWTriggers(tuple, loaded);
-					this.setActions(tuple, loaded);
-				}
-			}
-		}
-	}
+        try {
+            join(clazz, name, protocol, address, allowClones);
+        } catch (RepositoryException ex) {
+            LOG.error(Freedomotic.getStackTraceInfo(ex));
+        }
+    }
 
     /**
-     * Maps object behaviors to hardware triggers by calling {@link EnvObjectLogic#addTriggerMapping(Trigger, String)}
+     * Creates an {@link EnvObjectLogic} with the specification in input. If an
+     * {@link EnvObjectLogic} with the same protocol and address already exists
+     * it will exit with no changes.
+     *
+     * @param clazz the name of the thing template to load (eg: Light)
+     * @param name the name of the thing
+     * @param protocol the protocol which drives the thing
+     * @param address the address to uniquely identify the thing
+     * @param allowClones determines if an existent template can be cloned by
+     * adding an ordinal number to its name, true by default
+     * @return the thing created
+     * @throws com.freedomotic.exceptions.RepositoryException if it is not
+     * possible to retrieve the requested information
+     */
+    protected EnvObjectLogic join(String clazz, String name, String protocol, String address, boolean allowClones)
+            throws RepositoryException {
+        // check if autodiscovery can be applied
+        if (thingAlreadyExists(protocol, address)) {
+            LOG.info("A thing with protocol \"{}\" and address \"{}\" already exists in the environment. Autodiscovery exits without changes",
+                    protocol, address);
+            return null;
+        }
+
+        // if not allowed to clone an Thing
+        if (!allowClones && thingAlreadyExists(name)) {
+            LOG.info("A thing with name \"{}\" already exists in the environment. Autodiscovery exits without changes because property 'autodiscovery.allow-clones' property is ''{}'' for the received command",
+                    name, false);
+            return null;
+        }
+
+        // Check if the requested Thing template is loaded
+        ObjectPluginPlaceholder thingTemplate = (ObjectPluginPlaceholder) clientStorage.get(clazz);
+        if (thingTemplate == null) {
+            LOG.warn("Autodiscovery error: doesn't exist an object class called \"{}\"", clazz);
+            return null;
+        }
+
+        // start new Thing creation
+        LOG.warn("Autodiscovery request for an object called \"{}\" of type \"{}\"", name, clazz);
+        File templateFile = thingTemplate.getTemplate();
+        EnvObjectLogic loaded = thingsRepository.load(templateFile);
+
+        // changing the name and other properties invalidates related trigger
+        // and commands
+        // call init() again after this changes
+        if ((name != null) && !name.isEmpty()) {
+            loaded.getPojo().setName(name);
+        } else {
+            loaded.getPojo().setName(protocol);
+        }
+
+        loaded = thingsRepository.copy(loaded);
+        loaded.getPojo().setProtocol(protocol);
+        loaded.getPojo().setPhisicalAddress(address);
+        // Remove the 'virtual' tag and any other actAs configuration.
+        // TODO: it would be better to remove the actAs property and manage all
+        // with tags
+        loaded.getPojo().setActAs("");
+        loaded.setLocation(0, 0);
+        configureOptionalMapping(protocol, clazz, loaded);
+        LOG.info("Autodiscovery adds a thing called \"{}\" of type \"{}\"", loaded.getPojo().getName(), clazz);
+        return loaded;
+    }
+
+    /**
+     * Sets the PREFERRED MAPPING of the protocol plugin, if any is defined in
+     * its manifest. The mapping information will be asked to the plugin which
+     * implements the same protocol as the one specified in the arguments of
+     * this method.
+     * <p>
+     * The clazz parameter is used to identify a tuple in the plugin manifest.
+     * The configuration specified in this tuple will be applied to the target
+     * Thing.
+     *
+     * @param protocol the thing protocol, used to retrieve the corresponding
+     * plugin
+     * @param clazz the dot notation taxonomy used to identify the mapping that
+     * should be applied (eg: EnvObject.ElectricDevice.Light)
+     * @param loaded the target things
+     */
+    private void configureOptionalMapping(String protocol, String clazz, EnvObjectLogic loaded) {
+        Client addon = clientStorage.getClientByProtocol(protocol);
+
+        if ((addon != null) && (addon.getConfiguration().getTuples() != null)) {
+            for (int i = 0; i < addon.getConfiguration().getTuples().size(); i++) {
+                Map tuple = addon.getConfiguration().getTuples().getTuple(i);
+                String regex = (String) tuple.get("object.class");
+
+                if ((regex != null) && clazz.matches(regex)) {
+                    this.mapBehaviorsToHWTriggers(tuple, loaded);
+                    this.setActions(tuple, loaded);
+                }
+            }
+        }
+    }
+
+    /**
+     * Maps object behaviors to hardware triggers by calling
+     * {@link EnvObjectLogic#addTriggerMapping(Trigger, String)}
+     *
      * @param tuple Object with trigger name
-     * @param environmentLogic Environment logic on which to map trigger and behavior
+     * @param environmentLogic Environment logic on which to map trigger and
+     * behavior
      */
-	private void mapBehaviorsToHWTriggers(Map tuple, EnvObjectLogic environmentLogic) {
-		for (Behavior behavior : environmentLogic.getPojo().getBehaviors()) {
-			String triggerName = (String) tuple.get(behavior.getName());
-			if (triggerName != null) {
-				environmentLogic.addTriggerMapping(triggerRepository.findByName(triggerName).get(0), behavior.getName());
-			}
-		}
-	}
+    private void mapBehaviorsToHWTriggers(Map tuple, EnvObjectLogic environmentLogic) {
+        for (Behavior behavior : environmentLogic.getPojo().getBehaviors()) {
+            String triggerName = (String) tuple.get(behavior.getName());
+            if (triggerName != null) {
+                environmentLogic.addTriggerMapping(triggerRepository.findByName(triggerName).get(0), behavior.getName());
+            }
+        }
+    }
 
     /**
-     * Calls {@link EnvObjectLogic#setAction(String, Command)} with command name from <code>tuple</code>
+     * Calls {@link EnvObjectLogic#setAction(String, Command)} with command name
+     * from <code>tuple</code>
+     *
      * @param tuple Object with action names
      * @param environmentLogic Environment logic on which to set actions
      */
-	private void setActions(Map tuple, EnvObjectLogic environmentLogic) {
-		for (String action : environmentLogic.getPojo().getActions().stringPropertyNames()) {
-			String commandName = (String) tuple.get(action);
-			if (commandName != null) {
-				List<Command> list = commandRepository.findByName(commandName);
-				if (!list.isEmpty()) {
-					environmentLogic.setAction(action, list.get(0));
-				} else {
-					throw new FreedomoticRuntimeException("No commands found with name " + commandName);
-				}
-			}
-		}
-	}
+    private void setActions(Map tuple, EnvObjectLogic environmentLogic) {
+        for (String action : environmentLogic.getPojo().getActions().stringPropertyNames()) {
+            String commandName = (String) tuple.get(action);
+            if (commandName != null) {
+                List<Command> list = commandRepository.findByName(commandName);
+                if (!list.isEmpty()) {
+                    environmentLogic.setAction(action, list.get(0));
+                } else {
+                    throw new FreedomoticRuntimeException("No commands found with name " + commandName);
+                }
+            }
+        }
+    }
 
     /**
      * Checks if object already exists in <code>ThingRepository</code>
+     *
      * @param protocol Object protocol search criteria
      * @param address Object address search criteria
      * @return true if object already exists in repository
      */
-	private boolean thingAlreadyExists(String protocol, String address) {
-		// Multiple object with protocol 'unknown' are allowed
-		return !protocol.trim().equalsIgnoreCase("unknown") && (thingsRepository.findByAddress(protocol, address) != null);
-	}
+    private boolean thingAlreadyExists(String protocol, String address) {
+        // Multiple object with protocol 'unknown' are allowed
+        return !protocol.trim().equalsIgnoreCase("unknown") && (thingsRepository.findByAddress(protocol, address) != null);
+    }
 
     /**
      * Checks if object already exists in <code>ThingRepository</code>
+     *
      * @param name Object name search criteria
      * @return true if object already exists in repository
      */
-	private boolean thingAlreadyExists(String name) {
-		return !thingsRepository.findByName(name).isEmpty();
-	}
+    private boolean thingAlreadyExists(String name) {
+        return !thingsRepository.findByName(name).isEmpty();
+    }
 
     /**
      * Guaranteed to throw an exception
@@ -276,8 +276,8 @@ public final class Autodiscovery extends AbstractConsumer {
      * @throws UnsupportedOperationException always
      * @deprecated Unsupported operation.
      */
-	@Override
-	protected void onEvent(EventTemplate event) {
-		throw new UnsupportedOperationException("Autodiscovery module is not supposed to receive events");
-	}
+    @Override
+    protected void onEvent(EventTemplate event) {
+        throw new UnsupportedOperationException("Autodiscovery module is not supposed to receive events");
+    }
 }
