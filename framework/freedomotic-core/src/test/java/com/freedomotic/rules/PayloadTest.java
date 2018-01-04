31f5ddd776f0e6bffe9759e5ab42d2d8ec67332a
Mauro Cicolella <mcicolella@libero.it>
2018-01-04 15:49:19 +0100

    added test for new method in Payload class

--- framework/freedomotic-core/src/test/java/com/freedomotic/rules/PayloadTest.java
+++ framework/freedomotic-core/src/test/java/com/freedomotic/rules/PayloadTest.java
@@ -19,10 +19,10 @@
  */
 package com.freedomotic.rules;
 
+import com.freedomotic.events.MessageEvent;
 import org.slf4j.LoggerFactory;
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -68,7 +68,7 @@ public class PayloadTest {
     }
 
     /**
-     * Test of addStatement method, of class Payload.
+     * Test of addStatement() method of Payload class.
      */
     @Test
     public void testAddStatement() {
@@ -87,7 +87,7 @@ public class PayloadTest {
         //only a row is added
         assertEquals(preSize + 1, postSize);
         assertEquals(1, postSize);
-        //attribute and value must be exacly as in function attributes
+        //attribute and value must be the same as in function attributes
         assertEquals(payload.getStatements(attribute).get(0).getAttribute(), attribute);
         assertEquals(payload.getStatements(attribute).get(0).getValue(), value);
         //no null references
@@ -96,100 +96,100 @@ public class PayloadTest {
     }
 
     /**
-     * Test of equals method, of class Payload.
+     * Test of equals() method of Payload class.
      */
     @Test
     public void testEquals() {
-        //contruct the event
+        //construct the event
         Payload event = new Payload();
         event.addStatement(Statement.AND, "number", Statement.EQUALS, "1");
         event.addStatement(Statement.AND, "text", Statement.EQUALS, "abc");
         event.addStatement(Statement.AND, "testRegex", Statement.EQUALS, "EnvObject.ElectricDevice.Light");
 
-        //contruct the trigger
+        //construct the trigger
         Payload trigger = new Payload();
         trigger.addStatement(Statement.AND, "number", Statement.EQUALS, "1");
         trigger.addStatement(Statement.AND, "number", Statement.EQUALS, Statement.ANY);
-        //payload eveluation is after resolve step so the trigger has elready the event.* properties in it
+        //payload evaluation is after resolve step so the trigger has already the 'event.*' properties in it
         trigger.addStatement(Statement.AND, "event.number", Statement.EQUALS, "123");
         trigger.addStatement("SET", "defineANewProperty", Statement.EQUALS, "123");
 
         trigger.addStatement(Statement.AND, "testRegex", Statement.REGEX, "^EnvObject.ElectricDevice\\.(.*)");
 
         boolean expResult = true;
-        //compare trigger with events
+        //compare trigger to event
         boolean result = trigger.equals(event);
         assertEquals(expResult, result);
     }
 
     @Test
     public void testGreaterLess() {
-        //contruct the event
+        //construct the event
         Payload event = new Payload();
         event.addStatement(Statement.AND, "number", Statement.EQUALS, "1");
         event.addStatement(Statement.AND, "text", Statement.EQUALS, "abc");
 
-        //contruct the trigger
+        //construct the trigger
         Payload trigger = new Payload();
         trigger.addStatement(Statement.AND, "number", Statement.LESS_THAN, "2");
         trigger.addStatement(Statement.AND, "number", Statement.GREATER_THAN, "0");
         trigger.addStatement(Statement.AND, "number", Statement.LESS_EQUAL_THAN, "1");
 
-        //compare trigger with events
+        //compare trigger to event
         boolean result = trigger.equals(event);
         assertTrue("1 is greater than 0", result);
     }
 
     @Test
     public void testBetweenTime() {
-        LOG.info("Check if a give hour is inside a given hous interval");
-        //contruct the event
+        LOG.info("Check if a give hour is inside a given time interval");
+        //construct the event
         Payload event = new Payload();
         event.addStatement(Statement.AND, "morning", Statement.EQUALS, "10:00:00");
         event.addStatement(Statement.AND, "evening", Statement.EQUALS, "23:00:00");
         event.addStatement(Statement.AND, "midnight1", Statement.EQUALS, "00:00:00");
         event.addStatement(Statement.AND, "midnight2", Statement.EQUALS, "24:00:00");
 
-        //contruct the trigger
+        //construct the trigger
         Payload trigger = new Payload();
         trigger.addStatement(Statement.AND, "morning", Statement.BETWEEN_TIME, "9:00:0-11:00:00");
         trigger.addStatement(Statement.AND, "evening", Statement.BETWEEN_TIME, "22:00:00-8:00:00");
         trigger.addStatement(Statement.AND, "midnight1", Statement.BETWEEN_TIME, "23:30:00-0:30:00");
         trigger.addStatement(Statement.AND, "midnight2", Statement.BETWEEN_TIME, "23:30:00-0:30:00");
 
-        //compare trigger with events
+        //compare trigger to event
         boolean result = trigger.equals(event);
         assertTrue(result);
     }
 
     /**
-     * What if the trigger has a property which is not present in the event?
-     * Expected behavior is the trigger is not consistent with the event
+     * What if the trigger has a property not present in the event? Expected
+     * behavior is the trigger is not consistent with the event
      */
     @Test
     public void testTriggerHasUnexistentAttribute() {
-        LOG.info("Expected true if trigger has a statement with an attibute which "
+        LOG.info("Expected true if trigger has a statement with an attribute which "
                 + "doesn't exists in the related event payload.");
-        //contruct the event
+        //construct the event
         Payload event = new Payload();
         event.addStatement(Statement.AND, "number", Statement.EQUALS, "1");
         event.addStatement(Statement.AND, "text", Statement.EQUALS, "abc");
 
-        //contruct the trigger
+        //construct the trigger
         Payload trigger = new Payload();
         trigger.addStatement(Statement.AND, "unexistentPropertyOne", Statement.REGEX, "*");
         trigger.addStatement(Statement.AND, "unexistentPropertyTwo", Statement.EQUALS, "1");
-        //payload eveluation is after resolve step so the trigger has elready the event.* properties in it
+        //payload evaluation is after resolve step so the trigger has already the 'event.*' properties in it
         trigger.addStatement(Statement.AND, "event.number", Statement.EQUALS, "123");
         trigger.addStatement("SET", "defineANewProperty", Statement.EQUALS, "123");
 
-        //compare trigger with events
+        //compare trigger to event
         boolean result = trigger.equals(event);
         assertEquals(false, result);
     }
 
     /**
-     * Test of findAttribute method, of class Payload.
+     * Test of findAttribute() method of Payload class.
      */
     @Test
     public void testFindAttribute() {
@@ -203,5 +203,20 @@ public class PayloadTest {
         payload.addStatement("yes", "value6");
 
         assertEquals(4, payload.getStatements("yes").size());
+    }
+
+    /**
+     * Test of getStatementsAsJson() method of Payload class.
+     */
+    @Test
+    public void testGetStatementsAsJson() {
+        LOG.info("Produce a list of statements in JSON format");
+        Payload payload = new Payload();
+        payload.addStatement("no", "value1");
+        payload.addStatement("yes", "value2");
+        payload.addStatement("yes", "value3");
+
+        String value = "[{\"logical\":\"AND\",\"attribute\":\"no\",\"operand\":\"EQUALS\",\"value\":\"value1\"},{\"logical\":\"AND\",\"attribute\":\"yes\",\"operand\":\"EQUALS\",\"value\":\"value2\"},{\"logical\":\"AND\",\"attribute\":\"yes\",\"operand\":\"EQUALS\",\"value\":\"value3\"}]";
+        assertEquals(value, payload.getStatementsAsJson());
     }
 }
