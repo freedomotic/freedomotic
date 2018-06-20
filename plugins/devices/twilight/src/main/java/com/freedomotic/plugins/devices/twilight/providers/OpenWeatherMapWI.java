74bc7b65b79be60a558fda39fe1e19ea22a99dae
Mateus Paiva Matiazzi <mateuspaivamatiazzi1@gmail.com>
2018-06-20 01:07:45 -0400

    Mudança de Comentários

--- plugins/devices/twilight/src/main/java/com/freedomotic/plugins/devices/twilight/providers/OpenWeatherMapWI.java
+++ plugins/devices/twilight/src/main/java/com/freedomotic/plugins/devices/twilight/providers/OpenWeatherMapWI.java
@@ -1,23 +1,3 @@
-/**
- *
- * Copyright (c) 2009-2017 Freedomotic team http://freedomotic.com
- *
- * This file is part of Freedomotic
- *
- * This Program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2, or (at your option) any later version.
- *
- * This Program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Freedomotic; see the file COPYING. If not, see
- * <http://www.gnu.org/licenses/>.
- *
- */
 package com.freedomotic.plugins.devices.twilight.providers;
 
 import com.freedomotic.plugins.devices.twilight.WeatherInfo;
@@ -34,10 +14,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-/**
- *
- * @author Matteo Mazzoni
- */
 public class OpenWeatherMapWI implements WeatherInfo {
 
     private static final Logger LOG = LoggerFactory.getLogger(OpenWeatherMapWI.class.getCanonicalName());
